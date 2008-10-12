a6d457acca60eff2cd219357b774a06bcaaa2bec
jegelstaff <jegelstaff@e5624274-254a-0410-876d-c0e85fc73166>
2008-10-12 05:03:56 +0000

    3.0 RC1 Release
    git-svn-id: svn+ssh://svn.code.sf.net/p/formulize/code@21 e5624274-254a-0410-876d-c0e85fc73166

---
+++ iMenu/trunk/language/german/admin.php
@@ -0,0 +1,18 @@
+<?php
+define("_AM_IMENU_NEWLINK","Neuer Link");
+define("_AM_IMENU_TITLE","Titel");
+define("_AM_IMENU_LINK","Link");
+define("_AM_IMENU_HIDE","Verstecken");
+define("_AM_IMENU_SUBMIT","Abschicken");
+define("_AM_IMENU_WEIGHT","Position");
+define("_AM_IMENU_YES","Ja");
+define("_AM_IMENU_NO","Nein");
+define("_AM_IMENU_FUNCTIONS","Funktionen");
+define("_AM_IMENU_DEL","L&ouml;schen");
+define("_AM_IMENU_EDIT","Bearbeiten");
+define("_AM_IMENU_UP","Nach oben");
+define("_AM_IMENU_DOWN","Nach unten");
+define("_AM_IMENU_TARGET","Ziel");
+define("_AM_IMENU_GROUPS","Gruppen");
+define("_AM_IMENU_USERS","Mitglieder");
+?>
---
+++ iMenu/trunk/language/german/modinfo.php
@@ -0,0 +1,9 @@
+<?php
+
+define("_IM_IMENU_NAME","iMenu");
+
+define("_IM_IMENU_DESC","Ein nettes Postnuke-&auml;hnliches Linkmen&uuml;.");
+
+define("_IM_IMENU_ADMENU1","iMenu");
+
+?>
