4d9e0b79ea19746b5b383fde78797a4d94c75223
jegelstaff <jegelstaff@e5624274-254a-0410-876d-c0e85fc73166>
2008-10-12 05:04:58 +0000

    3.0 RC1 Release
    git-svn-id: svn+ssh://svn.code.sf.net/p/formulize/code@27 e5624274-254a-0410-876d-c0e85fc73166

---
+++ Pageworks/trunk/language/english/templates.php
@@ -0,0 +1,17 @@
+<?php
+
+// English strings for displaying information about this module in the templates
+define("_AM_PAGEWORKS_LANG_BLOCK_TITLE", "Useful Sites");
+define("_AM_PAGEWORKS_LANG_ERROR", "Error");
+define("_AM_PAGEWORKS_LANG_SAMPLE", "Some Sample Text");
+define("_AM_PAGEWORKS_LANG_WELCOME", "Welcome to pageworks");
+
+define("_NO_PERM", "You do not have permission to view this page.");
+
+// formulize yes, no constants used by extraction layer
+define("_formulize_TEMP_QYES", "Yes");
+define("_formulize_TEMP_QNO", "No");
+
+define("_pageworks_SAVE_BUTTON", "Save");
+
+?>
