dc01ae877b20662c5dfc87523bf01a25f1774c16
GibaPhp <GibaPhp@e5624274-254a-0410-876d-c0e85fc73166>
2008-11-17 10:08:16 +0000

    portuguesebr - iMenu - Started translation with base in folder english...
    git-svn-id: svn+ssh://svn.code.sf.net/p/formulize/code@74 e5624274-254a-0410-876d-c0e85fc73166

---
+++ iMenu/trunk/language/portuguesebr/admin.php
@@ -0,0 +1,27 @@
+<?php
+define("_IM_IMENUADMIN", "iMenu Administration");
+define("_IM_EDITIMENU", "Edit");
+define("_IM_NEWIMENU", "New Link");
+define("_IM_NEW","New Link");
+define("_IM_TITLE","Title");
+define("_IM_HIDE","Hide");
+define("_IM_TARGET","Target");
+define("_IM_GROUPS","Groups");
+define("_IM_GROUPS_HELP","If your link is to a Pageworks page or a Formulize form or screen, you do not need to specify the groups");
+define("_IM_LINK","Link");
+define("_IM_OPERATION","Functions");
+define("_IM_UP","Up");
+define("_IM_DOWN","Down");
+define("_IM_TARG_SELF","self");
+define("_IM_TARG_BLANK","blank");
+define("_IM_TARG_PARENT","parent");
+define("_IM_TARG_TOP","top");
+define("_IM_SUREDELETE","Are you sure you want to delete this iMenu record?");
+define("_IM_UPDATED","Database Updated Successfully!");
+define("_IM_NOTUPDATED","Could not update database!");
+define("_IM_SUBMIT", "Submit");
+
+define("_IM_PARENT", "Parent");
+define("_IM_NOPARENT", "No Parent");
+
+?>
