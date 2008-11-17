b6c8384b2beb36086713503c3038b78ec2dc20c9
GibaPhp <GibaPhp@e5624274-254a-0410-876d-c0e85fc73166>
2008-11-17 10:12:40 +0000

    portuguesebr - Reg_Codes - Started translation with base in folder english...
    git-svn-id: svn+ssh://svn.code.sf.net/p/formulize/code@76 e5624274-254a-0410-876d-c0e85fc73166

---
+++ Reg_Codes/trunk/language/portuguesebr/admin.php
@@ -0,0 +1,60 @@
+<?php
+// English strings for displaying information about this module in the site administration web pages
+
+define("_AM_REG_CODES_PERMDESC", "Select the groups which each group can issue codes for (users in a group will be able to invite new users and give them membership in the selected groups).");
+
+
+define("_AM_REG_CODES_CONFIGURE", "Configure Registration Codes");
+define("_AM_REG_CODES_EDIT", "Edit");
+define("_AM_REG_CODES_TITLE_ADMIN", "Registration Codes Administration");
+define("_AM_REG_CODES_CTITLE", "Configuration");
+define("_AM_REG_CODES_EMPTY", "The is nothing in the database.");
+
+define("_AM_REG_CODES_DESCRIPTION", "Registration Codes Description");
+
+// UI Labels and prompt Strings
+define("_AM_REG_CODES_LABEL_MAIN_TITLE", "Main Title for Registration Codes");
+define("_AM_REG_CODES_LABEL_CONFIG_MAIN_COUNT", "Number of Rows on Main Page.");
+define("_AM_REG_CODES_LABEL_CONFIG_MAIN_WHERE", "SQL Where clause for Main Page.");
+define("_AM_REG_CODES_LABEL_CONFIG_BLOCK_COUNT", "Number of Rows for block display.");
+define("_AM_REG_CODES_LABEL_CONFIG_BLOCK_WHERE", "Where clause for block display");
+
+// printf format strings
+define("_AM_REG_CODES_FMT_ERROR", "<P>ERROR: %s<BR>SQL: %s</P>\n");
+
+// Error Messages/Status Messages
+define("_AM_REG_CODES_ERR_ERROR", "Error");
+define("_AM_REG_CODES_OK_DB", "Database update succeeded.");
+define("_AM_REG_CODES_ERR_QUERY", "SQL");
+define("_AM_REG_CODES_ERR_REPLACE_FAILED", "Update failed.");
+define("_AM_REG_CODES_ERR_QUERY_FAILED", "Query failed.");
+
+// Buttons	
+define("_AM_REG_CODES_BUT_GO", "Go!");
+define("_AM_REG_CODES_BUT_SAVE", "Save");
+define("_AM_REG_CODES_BUT_EDIT", "Edit");
+define("_AM_REG_CODES_BUT_DELETE", "DELETE");
+
+// user manager
+define("_formulize_MODFORM_TITLE", "User Manager");
+define("_AM_MULTI_PERMISSIONS", "Select the groups you want to move/copy users from");
+define("_AM_MULTI_GROUP_LISTS", "Saved group lists");
+define("_AM_MULTI_CREATION_ORDER", "Creation order");
+define("_AM_MULTI_ALPHABETICAL_ORDER", "Alphabetical order");
+define("_AM_MULTI_GROUP_LISTS_NOSELECT", "No group list selected");
+define("_AM_MULTI_SAVE_LIST", "Save list");
+define("_AM_MULTI_DELETE_LIST", "Delete list");
+define("_formulize_SHOW_PERMS", "Move/copy these users now!");
+define("_AM_MULTI_GROUP_LIST_NAME", "Type a name for this list of groups");
+define("_AM_MULTI_GROUP_LIST_DELETE", "Do you really want to delete this list:");
+define("_AM_MULTI_DEST_GROUPS", "Select the groups you are moving/copying users to");
+define("_AM_MULTI_MOVECOPY", "Do you want to move users from one group to the other, or copy users so they remain in the original group?");
+define("_AM_MULTI_MOVE", "Move");
+define("_AM_MULTI_COPY", "Copy");
+define("_AM_MULTI_GROUP_CONFIRMMOVE", "You have selected to move users from one group to the other.  The 'source' group(s) will be empty and contain no users after doing this.  Are you sure you want to do this?");
+define("_AM_GROUP_MULTI_COMPLETE", "Operation complete!");
+
+
+
+
+?>
