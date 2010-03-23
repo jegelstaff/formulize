5a833b06c8bf933cfed80f263a60aa9220d7a5f7
jeffcote <jeffcote@e5624274-254a-0410-876d-c0e85fc73166>
2010-03-23 14:18:43 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/formulize/code@359 e5624274-254a-0410-876d-c0e85fc73166

---
+++ Formulize/trunk/include/formulize_quickselect.php
@@ -0,0 +1,16 @@
+<?php
+if( $_GET['q'] == 'a' )
+	print '[["Azilda",9]]';
+else if( $_GET['q'] == 'b' )
+	print '[["Bracebridge",18],["Brampton",13],["Burlington",17]]';
+else if( $_GET['q'] == 'c' )
+	print '[["Cambridge",15],["Chelmsford",8],["Coppercliff",16]]';
+else if( $_GET['q'] == 'd' )
+	print '[["Dowling",11]]';
+else if( $_GET['q'] == 'g' )
+	print '[[Guelph"",5]]';
+else if( $_GET['q'] == 'h' )
+	print '[["Hamilton",2]]';
+else if( $_GET['q'] == 'l' )
+	print '[["Lively",10],[London"",6]]';
+?>
