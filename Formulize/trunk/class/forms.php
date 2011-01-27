48cc0d65326a91aab72e455126b89c3f3d80e480
jegelstaff <jegelstaff@e5624274-254a-0410-876d-c0e85fc73166>
2011-01-27 22:09:03 +0000

    Improved support for {USER} keyword in per-group-filters (assigned via the permissions tab for a form).  You can now use {USER} as part of a larger string of text in a filter.
    git-svn-id: svn+ssh://svn.code.sf.net/p/formulize/code@452 e5624274-254a-0410-876d-c0e85fc73166

--- Formulize/trunk/class/forms.php
+++ Formulize/trunk/class/forms.php
@@ -714,7 +714,7 @@ class formulizeFormsHandler {
 			}
 
 			$likeBits = strstr(strtoupper($filterSettings[1][$i]), "LIKE") ? "%" : "";
-			$termToUse = $filterSettings[2][$i] === "{USER}" ? $uid : $filterSettings[2][$i];
+			$termToUse = str_replace("{USER}", $uid, $filterSettings[2][$i]); 
 			if (ereg_replace("[^A-Z{}]","", $termToUse) === "{TODAY}") {
 				$number = ereg_replace("[^0-9+-]","", $termToUse);
 				$termToUse = date("Y-m-d",mktime(0, 0, 0, date("m") , date("d")+$number, date("Y")));
