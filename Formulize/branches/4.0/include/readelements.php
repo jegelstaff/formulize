bf72375c0fe762ffbb1c3c0b87110949db9f7a46
jegelstaff <jegelstaff@e5624274-254a-0410-876d-c0e85fc73166>
2011-12-15 21:43:42 +0000

    Further updates to make sure derived values in a form that is part of a screen that overrides the current screen, will be updated properly.
    git-svn-id: svn+ssh://svn.code.sf.net/p/formulize/code@554 e5624274-254a-0410-876d-c0e85fc73166

--- Formulize/branches/4.0/include/readelements.php
+++ Formulize/branches/4.0/include/readelements.php
@@ -301,6 +301,26 @@ foreach($formulize_allWrittenEntryIds as $allWrittenFid=>$entries) {
 		}
 	}
 }
+
+// check for things that we should be updating based on the framework in effect for any override screen that has been declared
+if($_POST['overridescreen']) {
+	$override_screen_handler = xoops_getmodulehandler('screen', 'formulize');
+	$overrideScreenObject = $override_screen_handler->get($_POST['overridescreen']);
+	$overrideFrid = $overrideScreenObject->getVar('frid');
+	if($overrideFrid) {
+		$notUpdatedForms = array_diff($formulize_allWrittenFids, $formsUpdatedInFramework);	
+		foreach($notUpdatedForms as $thisFid) {
+			$formObject = $form_handler->get($thisFid);
+			if(array_search("derived", $formObject->getVar('elementTypes'))) {
+				foreach($formulize_allWrittenEntryIds[$thisFid] as $thisEntry) {
+					formulize_updateDerivedValues($thisEntry, $thisFid, $overrideFrid);
+					$formsUpdatedInFramework[$thisFid] = $thisFid;
+				}
+			}	
+		}
+	}
+}
+
 // check for any forms that were written, that did not have derived values updated as part of the framework
 if($frid) {
 	$notUpdatedForms = array_diff($formulize_allWrittenFids, $formsUpdatedInFramework);
