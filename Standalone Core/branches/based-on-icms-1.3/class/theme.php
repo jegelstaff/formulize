b741a96f0fc25b300ba5bf36f22880aa86094c5d
jegelstaff <jegelstaff@e5624274-254a-0410-876d-c0e85fc73166>
2012-06-03 20:53:38 +0000

    Fixed a backwards compatibility bug that was affecting code that relied on the XOOPS theme factory and createInstance method.
    git-svn-id: svn+ssh://svn.code.sf.net/p/formulize/code@649 e5624274-254a-0410-876d-c0e85fc73166

--- Standalone Core/branches/based-on-icms-1.3/class/theme.php
+++ Standalone Core/branches/based-on-icms-1.3/class/theme.php
@@ -24,8 +24,8 @@
 class xos_opal_ThemeFactory extends icms_view_theme_Factory {
 	private $_deprecated;
 	public function &createInstance($options = array(), $initArgs = array()) {
-		parent::createInstance($options, $initArgs);
 		$this->_deprecated = icms_core_Debug::setDeprecated('icms_view_theme_Factory', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
+		return parent::createInstance($options, $initArgs); // THIS LINE MOVED FROM ABOVE THE PREVIOUS LINE, AND RETURN ADDED.  BY FREEFORM SOLUTIONS JUNE 3 2012, FOR BACKWARDS COMPATIBILITY WITH CODE THAT RELIES ON THE XOOPS CLASS
 	}
 }
 
