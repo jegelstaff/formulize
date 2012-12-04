6ea779b135e97f638a7e89a7760cd60545160a63
jegelstaff <jegelstaff@e5624274-254a-0410-876d-c0e85fc73166>
2012-12-04 15:10:43 +0000

    Fixing issue related to Internet Explorer and lists of entries, where you couldn't execute searches with the Enter key...IE hasn't quite caught up to the rest of the web yet afterall (previous fix added a property that other browsers work with fine, but although it does now remove the button, it also renders the button inoperative!)
    git-svn-id: svn+ssh://svn.code.sf.net/p/formulize/code@747 e5624274-254a-0410-876d-c0e85fc73166

--- Formulize/trunk/include/entriesdisplay.php
+++ Formulize/trunk/include/entriesdisplay.php
@@ -921,7 +921,7 @@ function drawInterface($settings, $fid, $frid, $groups, $mid, $gperm_handler, $l
 	}
 	
 	if(strstr($_SERVER['HTTP_USER_AGENT'], "MSIE")) {
-		$submitButton = "<input type=submit name=submitx style=\"width:0px; height:0px; visibility: hidden;\" value='' ></input>\n";
+		$submitButton = "<input type=submit name=submitx style=\"width:0px; height:0px; border-width: 0px; padding: 0px;\" value='' ></input>\n";
 	} else {
 		$submitButton =  "<input type=submit name=submitx style=\"visibility: hidden;\" value='' ></input>\n";
 	}
