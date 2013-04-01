0a937f08ccb8883dc10010236e759853337b6a56
umsanter <umsanter@e5624274-254a-0410-876d-c0e85fc73166>
2013-04-01 17:31:00 +0000

    Might be the final submission.
    git-svn-id: svn+ssh://svn.code.sf.net/p/formulize/code@916 e5624274-254a-0410-876d-c0e85fc73166

--- joomla/2.5/formulize-joomla/admin/formulize.php
+++ joomla/2.5/formulize-joomla/admin/formulize.php
@@ -19,6 +19,9 @@ echo "<br /><br /><a href='".$_SERVER['PHP_SELF']."?option=com_formulize&sync=tr
 
 // if $_GET["sync"] exists, then do the sync operation and exit script.
 if ( isset($_GET["sync"]) ) {
+	$application = JFactory::getApplication();
+	$messages = "<br />";
+	$errors = "<br />";
 	require_once $params->get('formulize_path')."/integration_api.php";
 	jimport( 'joomla.access.access' );
 	$db = JFactory::getDbo();
@@ -38,21 +41,19 @@ if ( isset($_GET["sync"]) ) {
 	}
 	$exists = Formulize::getXoopsResourceID(0, $min_group_id+1);
 	if ( empty( $exists ) ) {
-		Formulize::createResourceMapping(0, $min_group_id+1, 2); // anonymous/public users
+		Formulize::createResourceMapping(0, $min_group_id+1, 2); // registered users
 	}
 	$exists = Formulize::getXoopsResourceID(0, $min_group_id+7);
 	if ( empty( $exists ) ) {
-		Formulize::createResourceMapping(0, $min_group_id+7, 1); // anonymous/public users
+		Formulize::createResourceMapping(0, $min_group_id+7, 1); // webmaster/super user
 	}
 
-	echo "<br></br><b>Syncing Joomla groups to the Formulize database</b><br />";
 	$query = $db->getQuery( true );
 	$query->select( array( 'id', 'title' ) )
 	->from( '#__usergroups' );
 
 	$db->setQuery( $query );
 	$list_of_groups = $db->loadObjectList();
-	
 	foreach ( $list_of_groups as &$group ) {
 		$group_data = array();
 		$group_data['groupid'] = $group->id;
@@ -61,27 +62,33 @@ if ( isset($_GET["sync"]) ) {
 
 		$exists = Formulize::getXoopsResourceID(0, $group->id);
 		if ($exists) {
-			Formulize::renameGroup($group_data['groupid'], $group_data['name']);
+			$flag = Formulize::renameGroup($group_data['groupid'], $group_data['name']);
+			// Display error message if necessary
+			if ($flag) {
+				$messages = $messages.'Group '.$group->title.': Group updated. <br />';
+			}
+			else {
+				$errors = $errors.'Group '.$group->title.' Error updating group.<br />';
+			}
 			continue;
 		}
 		else {
-			if ( Formulize::createGroup( $new_group ) ) {
-				echo "Group ".$group->title." created. <br />";
+			$flag = Formulize::createGroup( $new_group );
+			if ($flag) {
+				$messages = $messages."Group ".$group->title.": New group created. <br />";
 			}
 			else {
-				echo "Error creating group ".$group->title.".  <br />";
+				$errors = $errors.'Group '.$group->title.' Error creating group.<br />';
 			}
 		}
 	}
 
-	echo "<br /><b>Syncing Joomla users to the Formulize database</b><br />";
 	$query = $db->getQuery( true );
 	$query->select( array( 'id', 'username', 'name', 'email' ) )
 	->from( '#__users' );
 	$db->setQuery( $query );
 
 	$list_of_users = $db->loadObjectList();
-	
 	foreach ( $list_of_users as &$user ) {
 		// Create a new blank user for Formulize session
 		$user_data = array();
@@ -94,15 +101,14 @@ if ( isset($_GET["sync"]) ) {
 		// Create or update the user in Formulize
 		$exists = Formulize::getXoopsResourceID(1, $user_data['uid']);
 		$flag = NULL;
-		
 		if ( empty($exists) ) {// Create
 			$flag = Formulize::createUser( $new_user );
 			// Display error message if necessary
 			if ( !$flag ) {
-				echo 'User id: '.$user_data['uname'].': Error creating new user<br />';
+				$errors = $errors.'User '.$user_data['uname'].': Error creating new user<br />';
 			}
 			else {
-				echo 'User id: '.$user_data['uname'].': New user created. <br />';
+				$messages = $messages.'User '.$user_data['uname'].': New user created. <br />';
 				// add user to groups
 				$groups = JAccess::getGroupsByUser( $user->id );
 				foreach($groups as $group) {
@@ -113,12 +119,22 @@ if ( isset($_GET["sync"]) ) {
 			}
 		}
 		else { // Update
-			Formulize::updateUser( $user_data['uid'], $user_data );
+			$flag = Formulize::updateUser( $user_data['uid'], $user_data );
+			// Display error message if necessary
+			if ( !$flag ) {
+				$errors = $errors.'User '.$user_data['uid'].' Error updating new user<br />';
+			}
+			else {
+				$messages = $messages.'User '.$user_data['uname'].': User updated. <br />';
+			}
 		}
 	}
+	if ($messages != "<br />") {
+		$application->enqueueMessage(JText::_($messages));
+	}
 	
-	echo "<br /><b>Sync completed.</b><br />";
-	echo "<br /><a href='".$_SERVER['PHP_SELF']."?option=com_formulize'>Back to plugin configuration</a>";
+	if ($errors != "<br />") {
+		$application->enqueueMessage(JText::_($errors), 'error');
+	}
 }
-
 	
