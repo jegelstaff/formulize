1487c456992557bed0d74fe3d8005a4ddac3f8b9
aghoneim <aghoneim@e5624274-254a-0410-876d-c0e85fc73166>
2013-09-27 08:45:08 +0000

    I Updated the Import function to be able to handle Apps/Forms that got same ID as the DB.The File is free from Errors,however,it still need some tweaking to preform as intended.The Function Export Works fine but the half of the import function is not working which is Creat_Applications($filename).I will be debugging it tomorrow and fix it
    git-svn-id: svn+ssh://svn.code.sf.net/p/formulize/code@1011 e5624274-254a-0410-876d-c0e85fc73166

--- Formulize/trunk/class/Export_Import_App.php
+++ Formulize/trunk/class/Export_Import_App.php
@@ -1,12 +1,11 @@
 <?php
 include 'PDO_Conn.php';//Include the Connection File
 	
-	//$SQLStatments;
-	$SQLStatments=array();
+	
+	//Import();
+	//echo Check_Uniquines (3,1);
+	//Application Runs From Here:
 	Check_Post_Parameter();
-	//Global Array to hold Strings
-	//$Upload= new Upload();
-	//$Upload.load();
 	//Function To check if the Button Export or Import is Pressed and Loads the Function
 	Function Check_Post_Parameter()
 	{
@@ -39,7 +38,7 @@ include 'PDO_Conn.php';//Include the Connection File
 	$k1=1;
 	 $Links=Application_Fourm_Links($appid);
 	 	  
-	  $Insert="Insert INTO Prefix".$table." VALUES (";
+	  $Insert="Insert INTO Prefix".$table." VALUES (linkid,appid,fid) (";
 	 foreach ($Links as $key => $column) {
 	
 	 foreach($getfield as $k => $cur)
@@ -51,7 +50,7 @@ include 'PDO_Conn.php';//Include the Connection File
 	$Insert.=');';
 	array_push($SQLStatments,$Insert);
 	echo $Insert;
-	  $Insert="Insert INTO Prefix".$table." VALUES (";
+	  $Insert="Insert INTO Prefix".$table." VALUES (linkid,appid,fid) (";
 	}else {
 	$Insert.=$column[$cur['Field']];
 	$Insert.=',';
@@ -63,13 +62,10 @@ include 'PDO_Conn.php';//Include the Connection File
 	if ($k1==3){$k1=1;}
 	
 	}
-	 
-	// echo $Insert."<br/>";
-	//$Insert="Insert Values INTO Prefix".$table."(";
 	 }else if ($table=='_formulize_applications'){
 	 $k1=1;
 	 $Links=Application_Fourm_Links($appid);
-	  $Insert="Insert INTO Prefix".$table." VALUES (";
+	  $Insert="Insert INTO Prefix".$table." (appid, name,description) VALUES (";
 	 foreach ($Links as $key => $column) {
 	 $getapp=Get_App($column['appid']);
 	 foreach ($getapp as $key => $column) {
@@ -81,7 +77,7 @@ include 'PDO_Conn.php';//Include the Connection File
 	$Insert.=');';
 	array_push($SQLStatments,$Insert);
 	echo $Insert;
-	  $Insert="Insert INTO Prefix".$table." VALUES (";
+	  $Insert="Insert INTO Prefix".$table." (appid, name,description) VALUES (";
 	}else {
 	$Insert.=$column[$cur['Field']];
 	$Insert.=',';
@@ -94,7 +90,7 @@ include 'PDO_Conn.php';//Include the Connection File
 	 }else if ($table=='_formulize_id'){
 	 $k1=1;
 	 $Links=Application_Fourm_Links($appid);
-	  $Insert="Insert INTO Prefix".$table." VALUES (";
+	  $Insert="Insert INTO Prefix".$table." (`id_form`, `desc_form`, `singleentry`, `headerlist`, `tableform`, `lockedform`, `defaultform`, `defaultlist`, `menutext`, `form_handle`, `store_revisions`) VALUES (";
 	  foreach ($Links as $key => $column) {
 	 $getForm=Get_Form($column['fid']);
 	 foreach ($getForm as $key => $column) {
@@ -107,7 +103,7 @@ include 'PDO_Conn.php';//Include the Connection File
 	
 	array_push($SQLStatments,$Insert);
 	echo $Insert;
-	  $Insert="Insert INTO Prefix".$table." VALUES (";
+	  $Insert="Insert INTO Prefix".$table." (`id_form`, `desc_form`, `singleentry`, `headerlist`, `tableform`, `lockedform`, `defaultform`, `defaultlist`, `menutext`, `form_handle`, `store_revisions`) VALUES (";
 	}else {
 	$Insert.=$column[$cur['Field']];
 	$Insert.=',';
@@ -133,9 +129,10 @@ include 'PDO_Conn.php';//Include the Connection File
 	
 	
 	//Function to get the Fourms that's Linked with the requested AppID from URL
-	 function Application_Fourm_Links($appid)
+	 function Application_Fourm_Links($appid, $Uniq=null)
     {
 	
+		if ($Uniq==null){
 		$table=Prefix;
 		$table.='_formulize_application_form_link';
         $result =array();
@@ -150,12 +147,45 @@ include 'PDO_Conn.php';//Include the Connection File
         {
             $result[]=(array)$row;
         }
-        return $result;
+        
        
         $a= $Query->rowCount();
-		//echo $a;
-        //return $result;
-    }
+		}else {
+		if ($Uniq==1):{
+		$table=Prefix;
+		$table.='_formulize_application_form_link';
+        $result =array();
+		$conn=new Connection ();
+        $Query=$conn->connect()->prepare("SELECT COUNT( * ) AS num from ".$table." where linkid= :id") ;
+        $Query->bindValue(":id",$appid);
+        $Query->execute();
+		$result=$Query->fetch(\PDO::FETCH_ASSOC);
+		}elseif ($Uniq==2):
+		{
+		$table=Prefix;
+		$table.='_formulize_applications';
+        $result =array();
+		$conn=new Connection ();
+        $Query=$conn->connect()->prepare("SELECT COUNT( * ) AS num from ".$table." where appid= :id") ;
+        $Query->bindValue(":id",$appid);
+        $Query->execute();
+		$result=$Query->fetch(\PDO::FETCH_ASSOC);
+		}elseif  ($Uniq==3):
+		{
+				$table=Prefix;
+		$table.='_formulize_id';
+        $result =array();
+		$conn=new Connection ();
+        $Query=$conn->connect()->prepare("SELECT COUNT( * ) AS num from ".$table." where fid= :id") ;
+        $Query->bindValue(":id",$appid);
+        $Query->execute();
+		$result=$Query->fetch(\PDO::FETCH_ASSOC);
+		}endif;
+		
+		
+		
+		} return $result; }
+		
 	//Function to get the App by ID \\\\
 	Function Get_Link()
 	{
@@ -182,7 +212,7 @@ include 'PDO_Conn.php';//Include the Connection File
 	}
 	}
 	//Function to get the Application Rows by AppID
-	Function Get_App($appID)
+	Function Get_App($appID, $Uniq=null)
 	{
 	
 	$app_table=Prefix;
@@ -192,7 +222,6 @@ include 'PDO_Conn.php';//Include the Connection File
 	 $Query=$conn->connect()->prepare("select * from ".$app_table." where appid= :id") ;
 		$Query->bindValue(":id",$appID);
         $Query->execute();
-		
 		while ($row = $Query->fetch(\PDO::FETCH_OBJ))
         {
             $result[]=(array)$row;
@@ -203,7 +232,7 @@ include 'PDO_Conn.php';//Include the Connection File
         return $result;
 	}
 	//Function to get the Form Rows
-	function Get_Form($formID)
+	function Get_Form($formID, $Uniq=null)
 	
 	{
 	$Form_table=Prefix;
@@ -235,12 +264,8 @@ include 'PDO_Conn.php';//Include the Connection File
 
         }
 		return $result;
-		
-	
-	
 	}
 
-	
 	function Write_To_File()
 	{
 	header("Content-type: text/csv");
@@ -259,25 +284,186 @@ include 'PDO_Conn.php';//Include the Connection File
 	}
 	function Creat_Applications($filename)
 	{
+	echo "Hello";
+	$APP_ID_Replace;//This will store the New App ID if Needed
+	$Form_ID_Replace=array ();
 	//Some Error Handling will need to take place here and make sure not to overwrite anything in the DB
 	$file = fopen(".$filename.", "r");
 	while (!feof($file)) {
-	$getline = fgets($file);
+	$getlines = fgets($file);
+	$get_line=explode(";",$getline);
+	foreach ($get_line as $statement)
+   {
+   
+	if(strstr($subject, "_formulize_applications")) {
+	
+	preg_match('/\(\d\d|\(\d/', $statement, $matches);
+	$x1=explode('(',$matches[0]);
+	if (Check_Uniquines ($x1[1],2)==0)
+	{
 	$conn=new Connection ();
-	$Query=$conn->connect()->prepare($getline);
+	$Query=$conn->connect()->prepare($statement);
 	$Query->execute();
+	
+	}else {
+	//Do this $APP_ID_Replace =$matches1[0].New[];
+	$Se=preg_replace('/\(\d\d|\(\d/', "(", $statement);
+	$conn=new Connection ();
+	$Query=$conn->connect()->prepare($Se);
+	$Query->execute();
+	$Query=$conn->connect()->prepare("SELECT max(appid) FROM ".Prefix."_formulize_applications");
+	$Query->execute();
+	$result=$Query->fetch(\PDO::FETCH_ASSOC);
+	$APP_ID_Replace=$matches1[0].":".$result['appid'];
+	
+    }}
+	if(strstr($subject, "_formulize_id")) {
+	 preg_match('/\(\d\d|\(\d/', $statement, $matches);
+	$x1=explode('(',$matches[0]);
+	if (Check_Uniquines ($x1[1],3)==0)
+	{
+	$conn=new Connection ();
+	$Query=$conn->connect()->prepare($statement);
+	$Query->execute();
+	
+	}else {
+	//Do this 
+	$Se=preg_replace('/\(\d\d|\(\d/', "(", $statement);
+	$conn=new Connection ();
+	$Query=$conn->connect()->prepare($Se);
+	$Query->execute();
+	$Query=$conn->connect()->prepare("SELECT max(id_form) FROM ".Prefix."_formulize_id");
+	$Query->execute();
+	$result=$Query->fetch(\PDO::FETCH_ASSOC);
+	$matches1[0].":".$result['appid'];
+	$Query=$conn->connect()->prepare("INSERT INTO form_mapping VALUES (:id,id2)");
+	$Query->bindValue(":id",$matches1[0]);
+	$Query->bindValue(":id2",$result['id_form']);
+	$Query->execute();
+    }
+	}
+	}
+	//Here is Different than the Above Statments//Because we need to link the New ID if the fourms/App has been updated
+	foreach ($get_line as $statement){
+	if(strstr($subject, "_formulize_application_form_link")) {
+	preg_match('/\(\d\d|\(\d/', $statement, $matches);
+	$x1=explode('(',$matches[0]);
+	if (Check_Uniquines ($x1[1],1)==0)
+	{ /////////
+	$APP_ID_Replace;//This will store the New App ID if Needed
+	$Form_ID_Replace=array ();
+	if (empty($APP_ID_Replace))
+	{ 
+		if (empty ($Form_ID_Replace)){
+		$conn=new Connection ();
+		$Query=$conn->connect()->prepare($statement);
+		$Query->execute();}
+		else{  
+		$result =array();
+		$conn=new Connection ();
+        $Query=$conn->connect()->prepare("SELECT N_FormID from form_mapping where O_FomID= :id") ;
+        $Query->bindValue(":id",$Fid);
+        $Query->execute();
+		$result=$Query->fetch(\PDO::FETCH_ASSOC);
+		$ss2=preg_replace('/(,\d\d)\)|(,\d)\)/', ",".$result['N_FormID'].")",$statement );
+		$Query=$conn->connect()->prepare($ss2);
+		$Query->execute();
+	}
+	}else {
+	$aPP_id_2=explode (":",$APP_ID_Replace);
+	$ss2=preg_replace('/(,\d\d,)|(,\d,)/', ",".$aPP_id_2[1].",", $statement);
+	if (empty ($Form_ID_Replace)){
+	$conn=new Connection ();
+	$Query=$conn->connect()->prepare($ss2);
+	$Query->execute();
+	}else
+	{
+	$result =array();
+		$conn=new Connection ();
+        $Query=$conn->connect()->prepare("SELECT N_FormID from form_mapping where O_FomID= :id") ;
+        $Query->bindValue(":id",$Fid);
+        $Query->execute();
+		$result=$Query->fetch(\PDO::FETCH_ASSOC);
+		$ss2=preg_replace('/(,\d\d)\)|(,\d)\)/', ",".$result['N_FormID'].")",$statement );
+		$Query=$conn->connect()->prepare($ss2);
+		$Query->execute();
+	}
+	}
+	}else {
+	//Use the Auto Increment  
+	$ee=preg_replace('/\(\d\d|\(\d/', "(", $statement);
+		if (empty($APP_ID_Replace))
+	{ 
+		if (empty ($Form_ID_Replace)){
+		$conn=new Connection ();
+		$Query=$conn->connect()->prepare($ee);
+		$Query->execute();}
+		else{  
+		$result =array();
+		$conn=new Connection ();
+        $Query=$conn->connect()->prepare("SELECT N_FormID from form_mapping where O_FomID= :id") ;
+        $Query->bindValue(":id",$Fid);
+        $Query->execute();
+		$result=$Query->fetch(\PDO::FETCH_ASSOC);
+		$ss2=preg_replace('/(,\d\d)\)|(,\d)\)/', ",".$result['N_FormID'].")",$ee );
+		$Query=$conn->connect()->prepare($ss2);
+		$Query->execute();
+	}
+	}else {
+	$aPP_id_2=explode (":",$APP_ID_Replace);
+	$epa=preg_replace('/(,\d\d,)|(,\d,)/', ",".$aPP_id_2[1].",", $ee);
+	if (empty ($Form_ID_Replace)){
+	$conn=new Connection ();
+	$Query=$conn->connect()->prepare($epa);
+	$Query->execute();
+	}else
+	{
+	$result =array();
+		$conn=new Connection ();
+        $Query=$conn->connect()->prepare("SELECT N_FormID from form_mapping where O_FomID= :id") ;
+        $Query->bindValue(":id",$Fid);
+        $Query->execute();
+		$result=$Query->fetch(\PDO::FETCH_ASSOC);
+		$ss22=preg_replace('/(,\d\d)\)|(,\d)\)/', ",".$result['N_FormID'].")",$epa );
+		$Query=$conn->connect()->prepare($ss22);
+		$Query->execute();
+	}
+	}
+	}
+	
+    }
+	
+	}
 	fclose($file);
 	}
 	}
+	
+
 	Function Import ()
 	{
-	
+				
 				require_once "upload.php";
 				$filename = '/Upload/'.UploadFile;
-				//As soon it loads the file it changes the PREFIX word in the file to the DB Prefix 
+				//As soon it loads the file it changes the PREFIX word in the file to the current DB Prefix 
 				replaces_Prefix_in_file ($filename);
 				Creat_Applications($filename);
 	
+	}
+	Function Check_Uniquines ($ID,$field)
+	{
+	$Check;
+	switch ($field){
+	case 1:
+	$Check=Application_Fourm_Links($ID,1);
+	break;
+	case 2:
+	$Check=Application_Fourm_Links($ID,2);
+	break;
+	case 3:
+	$Check=Application_Fourm_Links($ID,3);
+	break;
+	}
+	return $Check['num'];
 	}
 
 
