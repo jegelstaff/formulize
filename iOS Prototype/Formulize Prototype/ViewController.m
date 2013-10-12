34269aad55c9177f1ea05b57ab7305972a43851a
mnelly <mnelly@e5624274-254a-0410-876d-c0e85fc73166>
2013-10-12 00:26:59 +0000

    Importing Formulize Prototype
    git-svn-id: svn+ssh://svn.code.sf.net/p/formulize/code@1038 e5624274-254a-0410-876d-c0e85fc73166

---
+++ iOS Prototype/Formulize Prototype/ViewController.m
@@ -0,0 +1,89 @@
+//
+//  ViewController.m
+//  Formulize Prototype
+//
+//  Created by Mary Nelly on 10-11-13.
+//  Copyright (c) 2013 Laurentian University. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+@synthesize signinButton;
+@synthesize applicationData;
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    applicationData = [[NSArray alloc] initWithObjects:@"Wildlife Monitoring",@"Zoo Animal Caretaking", nil];
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [self setSigninButton:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+#pragma mark - TableView Data Source Methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [applicationData count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = nil;
+    
+    cell = [tableView dequeueReusableCellWithIdentifier:@"appCells"];
+    
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"appCells"];
+    }
+    cell.textLabel.text = [applicationData objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+
+
+@end
---
+++ iOS Prototype/Formulize Prototype/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Formulize Prototype
+//
+//  Created by Mary Nelly on 10-11-13.
+//  Copyright (c) 2013 Laurentian University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
