00db7a4afa7d8aa78e1c03d5d2149fcb151a6390
Julian Egelstaff <nospam@yourturn.ca>
2013-11-05 12:28:39 -0500

    Fixing replacement of { } terms in quicksearches
    They will only be replaced if the entire term is a { } term, ie:
    OR{something} will be ignored...we only want to catch simple situations
    for now for the users.

--- modules/formulize/include/entriesdisplay.php
+++ modules/formulize/include/entriesdisplay.php
@@ -510,19 +510,21 @@ function displayEntries($formframe, $mainform="", $loadview="", $loadOnlyView=0,
 		// remove any { } terms that don't have a passed in value (so they appear as "" to users)
 		// only deal with terms that start and end with { } and not ones where the { } terms is not the entire term
 		if(substr($v, 0, 1) == "{" AND substr($v, -1) == "}" AND substr($k, 0, 7) == "search_" AND in_array(substr($k, 7), $showcols)) {
-			$activeViewId = substr($settings['lastloaded'], 1); // will have a p in front of the number, to show it's a published view (or an s, but that's unlikely to ever happen in this case)
-			$ownerOfLastLoadedViewData = q("SELECT sv_owner_uid FROM " . $xoopsDB->prefix("formulize_saved_views") . " WHERE sv_id=".intval($activeViewId));
-			$ownerOfLastLoadedView = $ownerOfLastLoadedViewData[0]['sv_owner_uid'];
-			if(!$update_other_reports AND $uid != $ownerOfLastLoadedView) {
-				$requestKeyToUse = substr($v,1,-1);
-				if(isset($_POST[$requestKeyToUse])) {
-					$_POST[$k] = $_POST[$requestKeyToUse];
-				} elseif(isset($_GET[$requestKeyToUse])) {
-					$_POST[$k] = $_GET[$requestKeyToUse];
-				} elseif($v == "{USER}" AND $xoopsUser) {
-					$_POST[$k] = $xoopsUser->getVar('name') ? $xoopsUser->getVar('name') : $xoopsUser->getVar('uname');
-				} elseif(!strstr($v, "{BLANK}") AND !strstr($v, "{TODAY") AND !strstr($v, "{PERGROUPFILTER}") AND !strstr($v, "{USER")) { 
-					unset($_POST[$k]); // clear terms where no match was found, because this term is not active on the current page, so don't confuse users by showing it
+			$requestKeyToUse = substr($v,1,-1);
+			if(!strstr($requestKeyToUse,"}") AND !strstr($requestKeyToUse, "{")) { // double check that there's no other { } in the term!
+				$activeViewId = substr($settings['lastloaded'], 1); // will have a p in front of the number, to show it's a published view (or an s, but that's unlikely to ever happen in this case)
+				$ownerOfLastLoadedViewData = q("SELECT sv_owner_uid FROM " . $xoopsDB->prefix("formulize_saved_views") . " WHERE sv_id=".intval($activeViewId));
+				$ownerOfLastLoadedView = $ownerOfLastLoadedViewData[0]['sv_owner_uid'];
+				if(!$update_other_reports AND $uid != $ownerOfLastLoadedView) {
+					if(isset($_POST[$requestKeyToUse])) {
+						$_POST[$k] = $_POST[$requestKeyToUse];
+					} elseif(isset($_GET[$requestKeyToUse])) {
+						$_POST[$k] = $_GET[$requestKeyToUse];
+					} elseif($v == "{USER}" AND $xoopsUser) {
+						$_POST[$k] = $xoopsUser->getVar('name') ? $xoopsUser->getVar('name') : $xoopsUser->getVar('uname');
+					} elseif(!strstr($v, "{BLANK}") AND !strstr($v, "{TODAY") AND !strstr($v, "{PERGROUPFILTER}") AND !strstr($v, "{USER")) { 
+						unset($_POST[$k]); // clear terms where no match was found, because this term is not active on the current page, so don't confuse users by showing it
+					}
 				}
 			}
 		}
