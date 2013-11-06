8c3a61c938330ee4d098f2616f9b11c9c826bb7f
Julian Egelstaff <nospam@yourturn.ca>
2013-11-06 17:42:10 -0500

    Test 1

--- modules/formulize/initialize.php
+++ modules/formulize/initialize.php
@@ -33,6 +33,8 @@
 ##  Project: Formulize                                                       ##
 ###############################################################################
 
+// futher testing
+
 if (file_exists(XOOPS_ROOT_PATH.'/class/mail/phpmailer/class.phpmailer.php'))
     include_once XOOPS_ROOT_PATH.'/class/mail/phpmailer/class.phpmailer.php';
 
