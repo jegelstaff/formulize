767a9befbb620e9eaaa26ab40defe3b3a3c3f95a
Alex <alexander,ray.boyd@gmail.com>
2014-03-23 19:27:56 -0700

    fixing an issue where when using custom templates it was drawing entire table one per table value.

--- modules/formulize/include/entriesdisplay.php
+++ modules/formulize/include/entriesdisplay.php
@@ -1355,7 +1355,7 @@ function drawEntries($fid, $cols, $searches="", $frid="", $scope, $standalone=""
 
 	} 
 	// MASTER HIDELIST CONDITIONAL...
-	if(!$settings['hlist'] AND !$listTemplate) {
+	if(!$settings['hlist']) {
 		print "<div class=\"list-of-entries-container\"><table class=\"outer\">";
 
 		$count_colspan = count($cols)+1;
@@ -1432,10 +1432,11 @@ function drawEntries($fid, $cols, $searches="", $frid="", $scope, $standalone=""
     /*print "start: $formulize_LOEPageStart<br>";
     print "size: $formulize_LOEPageSize<br>";
     print "actualsize: $actualPageSize<br>";*/
-		if(isset($data)) {
+	if(isset($data)) {
 			//for($entryCounter=$formulize_LOEPageStart;$entryCounter<$actualPageSize;$entryCounter++) {
-      foreach($data as $id=>$entry) {
-        formulize_benchmark("starting to draw one row of results");
+		foreach($data as $id=>$entry) {
+			formulize_benchmark("starting to draw one row of results");
+			
 				//$entry = $data[$entryCounter];
 				//$id=$entryCounter;
 						
@@ -1492,189 +1493,118 @@ function drawEntries($fid, $cols, $searches="", $frid="", $scope, $standalone=""
 							print "</center></p>\n";
 						}
 					} // end of IF NO LOCKCONTROLS
-					if($useViewEntryLinks OR $useCheckboxes != 2) {
-						print "</td>\n";
-					}	
-			
-					$column_counter = 0;
-					
-					if($columnWidth) {
-						$columnWidthParam = "style=\"width: $columnWidth" . "px\"";
-					} else {
-						$columnWidthParam = "";
+			if($listTemplate)
+			{
+				include XOOPS_ROOT_PATH."/modules/formulize/templates/screens/default/".$screen->getVar('sid')."/listtemplate.php";
+			}
+			else
+			{
+				if($useViewEntryLinks OR $useCheckboxes != 2) {
+					print "</td>\n";
+				}	
+		
+				$column_counter = 0;
+				
+				if($columnWidth) {
+					$columnWidthParam = "style=\"width: $columnWidth" . "px\"";
+				} else {
+					$columnWidthParam = "";
+				}
+				
+				for($i=0;$i<count($cols);$i++) {
+				//formulize_benchmark("drawing one column");
+					$col = $cols[$i];
+					$colhandle = $settings['columnhandles'][$i];
+					$classToUse = $class . " column column".$i;
+					$cellRowAddress = $id+2;
+					if($i==0) {
+						print "<td $columnWidthParam class=\"$class floating-column\" id='floatingcelladdress_$cellRowAddress'>\n";
 					}
-					
-          for($i=0;$i<count($cols);$i++) {
-            //formulize_benchmark("drawing one column");
-						$col = $cols[$i];
-						$colhandle = $settings['columnhandles'][$i];
-						$classToUse = $class . " column column".$i;
-						$cellRowAddress = $id+2;
-						if($i==0) {
-							print "<td $columnWidthParam class=\"$class floating-column\" id='floatingcelladdress_$cellRowAddress'>\n";
-						}
-						print "<td $columnWidthParam class=\"$classToUse\" id=\"celladdress_".$cellRowAddress."_".$i."\">\n";
-						if($col == "creation_uid" OR $col == "mod_uid") {
-							$userObject = $member_handler->getUser(display($entry, $col));
-							if($userObject) {
-								$nameToDisplay = $userObject->getVar('name') ? $userObject->getVar('name') : $userObject->getVar('uname');
-							} else {
-								$nameToDisplay = _FORM_ANON_USER;
-							}
-							$value = "<a href=\"" . XOOPS_URL . "/userinfo.php?uid=" . display($entry, $col) . "\" target=_blank>" . $nameToDisplay . "</a>";
+					print "<td $columnWidthParam class=\"$classToUse\" id=\"celladdress_".$cellRowAddress."_".$i."\">\n";
+					if($col == "creation_uid" OR $col == "mod_uid") {
+						$userObject = $member_handler->getUser(display($entry, $col));
+						if($userObject) {
+							$nameToDisplay = $userObject->getVar('name') ? $userObject->getVar('name') : $userObject->getVar('uname');
 						} else {
-							$value = display($entry, $col);
+							$nameToDisplay = _FORM_ANON_USER;
 						}
+						$value = "<a href=\"" . XOOPS_URL . "/userinfo.php?uid=" . display($entry, $col) . "\" target=_blank>" . $nameToDisplay . "</a>";
+					} else {
+						$value = display($entry, $col);
+					}
 
-                        // set in the display function, corresponds to the entry id of the record in the form where the current value was retrieved from.  If there is more than one local entry id, because of a one to many framework, then this will be an array that corresponds to the order of the values returned by display.
-                        $currentColumnLocalId = $GLOBALS['formulize_mostRecentLocalId'];
-                        // if we're supposed to display this column as an element... (only show it if they have permission to update this entry)
-                        if (in_array($colhandle, $deColumns) and formulizePermHandler::user_can_edit_entry($fid, $uid, $entry)) {
-							include_once XOOPS_ROOT_PATH . "/modules/formulize/include/elementdisplay.php";
-							if($frid) { // need to work out which form this column belongs to, and use that form's entry ID.  Need to loop through the entry to find all possible internal IDs, since a subform situation would lead to multiple values appearing in a single cell, so multiple displayElement calls would be made each with their own internal ID.
-								foreach($entry as $entryFormHandle=>$entryFormData) {
-									foreach($entryFormData as $internalID=>$entryElements) {
-										$deThisIntId = false;
-										foreach($entryElements as $entryHandle=>$values) {
-											if($entryHandle == $col) { // we found the element that we're trying to display
-												if($deThisIntId) { print "\n<br />\n"; } // could be a subform so we'd display multiple values
-												if($deDisplay) {
-													print '<div id="deDiv_'.$colhandle.'_'.$internalID.'">';
-													print getHTMLForList($values, $colhandle, $internalID, $deDisplay, $textWidth, $currentColumnLocalId, $fid, $cellRowAddress, $i);
-													print "</div>";
-												} else {
-													displayElement("", $colhandle, $internalID);
-												}
-												$deThisIntId = true;
+		// set in the display function, corresponds to the entry id of the record in the form where the current value was retrieved from.  If there is more than one local entry id, because of a one to many framework, then this will be an array that corresponds to the order of the values returned by display.
+		$currentColumnLocalId = $GLOBALS['formulize_mostRecentLocalId'];
+		// if we're supposed to display this column as an element... (only show it if they have permission to update this entry)
+		if (in_array($colhandle, $deColumns) and formulizePermHandler::user_can_edit_entry($fid, $uid, $entry)) {
+						include_once XOOPS_ROOT_PATH . "/modules/formulize/include/elementdisplay.php";
+						if($frid) { // need to work out which form this column belongs to, and use that form's entry ID.  Need to loop through the entry to find all possible internal IDs, since a subform situation would lead to multiple values appearing in a single cell, so multiple displayElement calls would be made each with their own internal ID.
+							foreach($entry as $entryFormHandle=>$entryFormData) {
+								foreach($entryFormData as $internalID=>$entryElements) {
+									$deThisIntId = false;
+									foreach($entryElements as $entryHandle=>$values) {
+										if($entryHandle == $col) { // we found the element that we're trying to display
+											if($deThisIntId) { print "\n<br />\n"; } // could be a subform so we'd display multiple values
+											if($deDisplay) {
+												print '<div id="deDiv_'.$colhandle.'_'.$internalID.'">';
+												print getHTMLForList($values, $colhandle, $internalID, $deDisplay, $textWidth, $currentColumnLocalId, $fid, $cellRowAddress, $i);
+												print "</div>";
+											} else {
+												displayElement("", $colhandle, $internalID);
 											}
+											$deThisIntId = true;
 										}
 									}
 								}
-							} else { // display based on the mainform entry id
-								if($deDisplay) {
-									print '<div id="deDiv_'.$colhandle.'_'.$linkids[0].'">';
-									print getHTMLForList($value,$colhandle,$linkids[0], $deDisplay, $textWidth, $currentColumnLocalId, $fid, $cellRowAddress, $i);
-									print "</div>";
-								} else {
-									displayElement("", $colhandle, $linkids[0]); // works for mainform only!  To work on elements from a framework, we need to figure out the form the element is from, and the entry ID in that form, which is done above
-								}
 							}
-							$GLOBALS['formulize_displayElement_LOE_Used'] = true;
-						} elseif($col != "creation_uid" AND $col!= "mod_uid") {
-							print getHTMLForList($value, $col, $linkids[0], 0, $textWidth, $currentColumnLocalId, $fid, $cellRowAddress, $i);
-						} else { // no special formatting on the uid columns:
-							print $value;
+						} else { // display based on the mainform entry id
+							if($deDisplay) {
+								print '<div id="deDiv_'.$colhandle.'_'.$linkids[0].'">';
+								print getHTMLForList($value,$colhandle,$linkids[0], $deDisplay, $textWidth, $currentColumnLocalId, $fid, $cellRowAddress, $i);
+								print "</div>";
+							} else {
+								displayElement("", $colhandle, $linkids[0]); // works for mainform only!  To work on elements from a framework, we need to figure out the form the element is from, and the entry ID in that form, which is done above
+							}
 						}
-						
-						print "</td>\n";
-						$column_counter++;
+						$GLOBALS['formulize_displayElement_LOE_Used'] = true;
+					} elseif($col != "creation_uid" AND $col!= "mod_uid") {
+						print getHTMLForList($value, $col, $linkids[0], 0, $textWidth, $currentColumnLocalId, $fid, $cellRowAddress, $i);
+					} else { // no special formatting on the uid columns:
+						print $value;
 					}
 					
-					// handle inline custom buttons
-					foreach($inlineButtons as $caid=>$thisCustomAction) {
-						list($caCode) = processCustomButton($caid, $thisCustomAction, $linkids[0], $entry); // only bother with the code, since we already processed any clicked button above
-						if($caCode) {
-							print "<td $columnWidthParam class=$class>\n";
-							print "<center>$caCode</center>\n";
-							print "</td>\n";
-						}
-					}
-					
-					// handle hidden elements for passing back to custom buttons
-					foreach($hiddenColumns as $thisHiddenCol) {
-						print "\n<input type=\"hidden\" name=\"hiddencolumn_".$linkids[0]."_$thisHiddenCol\" value=\"" . htmlspecialchars(display($entry, $thisHiddenCol)) . "\"></input>\n";
-					}				
-					
-					print "</tr>\n";
+					print "</td>\n";
+					$column_counter++;
+				}
 				
-				} else { // this is a blank entry
-					$blankentries++;
-				} // end of not "" check
+				// handle inline custom buttons
+				foreach($inlineButtons as $caid=>$thisCustomAction) {
+					list($caCode) = processCustomButton($caid, $thisCustomAction, $linkids[0], $entry); // only bother with the code, since we already processed any clicked button above
+					if($caCode) {
+						print "<td $columnWidthParam class=$class>\n";
+						print "<center>$caCode</center>\n";
+						print "</td>\n";
+					}
+				}
+				
+				// handle hidden elements for passing back to custom buttons
+				foreach($hiddenColumns as $thisHiddenCol) {
+					print "\n<input type=\"hidden\" name=\"hiddencolumn_".$linkids[0]."_$thisHiddenCol\" value=\"" . htmlspecialchars(display($entry, $thisHiddenCol)) . "\"></input>\n";
+				}				
+				
+				print "</tr>\n";
 			
-			} // end of foreach data as entry
-		} // end of if there is any data to draw
+				}
+			}
+			else { // this is a blank entry
+				$blankentries++;
+			} // end of not "" check
+			
+			
+		} // end of foreach data as entry
+	} // end of if there is any data to draw
 
 		print "</table></div>";
-	} elseif($listTemplate AND !$settings['hlist']) {
-
-		// USING A CUSTOM LIST TEMPLATE SO DO EVERYTHING DIFFERENTLY
-		// print str_replace("\n", "<br />", $listTemplate); // debug code
-		$mainFormHandle = key($data[key($data)]);
-		$formulize_LOEPageStart = (isset($_POST['formulize_LOEPageStart']) AND !$regeneratePageNumbers) ? intval($_POST['formulize_LOEPageStart']) : 0;
-		$actualPageSize = $formulize_LOEPageSize ? $formulize_LOEPageStart + $formulize_LOEPageSize : $GLOBALS['formulize_countMasterResultsForPageNumbers'];
-		if(strstr($listTemplate, "displayElement")) {
-			include_once XOOPS_ROOT_PATH . "/modules/formulize/include/elementdisplay.php";
-		}
-		if(isset($data)) {
-			//for($entryCounter=$formulize_LOEPageStart;$entryCounter<$actualPageSize;$entryCounter++) {
-			
-			// setup the view name variables, with true only set for the last loaded view
-			$viewNumber = 1;
-			foreach($settings['publishedviewnames'] as $id=>$thisViewName) {
-				$thisViewName = str_replace(" ", "_", $thisViewName);
-				if($id == $settings['lastloaded']) {
-					${$thisViewName} = true;
-					${'view'.$viewNumber} = true;
-				} else {
-					${$thisViewName} = false;
-					$view{'view'.$viewNumber} = false;
-				}
-				$viewNumber++;
-			}
-			
-      foreach($data as $id=>$entry) {
-				//$entry = $data[$entryCounter];
-				//$id=$entryCounter;
-						
-				// check to make sure this isn't an unset entry (ie: one that was blanked by the extraction layer just prior to sending back results
-				// Since the extraction layer is unsetting entries to blank them, this condition should never be met?
-				// If this condition is ever met, it may very well screw up the paging of results!
-				// NOTE: this condition is met on the last page of a paged set of results, unless the last page as exactly the same number of entries on it as the limit of entries per page
-				if($entry != "") { 
-
-					// Set up the variables for the link to the current entry, and the checkbox that can be used to select the current entry
-					$linkids = internalRecordIds($entry, $mainFormHandle);
-					$entry_id = $linkids[0]; // make a nice way of referring to this for in the eval'd code
-					$form_id = $fid; // make a nice way of referring to this for in the eval'd code
-					if(!$settings['lockcontrols']) { //  AND !$loadview) { // -- loadview removed from this function sept 24 2005
-						$viewEntryLinkCode = "<a href='" . $currentURL;
-						if(strstr($currentURL, "?")) { // if params are already part of the URL...
-							$viewEntryLinkCode .= "&";
-						} else {
-							$viewEntryLinkCode .= "?";
-						}
-                        $viewEntryLinkCode .= "ve=" . $entry_id . "' onclick=\"javascript:goDetails('" . $entry_id . "');return false;\">";
-                        $GLOBALS['formulize_viewEntryId'] = $entry_id;
-                        // put into global scope so the function 'viewEntryLink' can pick it up if necessary
-                        $GLOBALS['formulize_viewEntryLinkCode'] = $viewEntryLinkCode;
-
-                        // check to see if we should draw in the delete checkbox
-			// 2 is none, 1 is all
-                        if ($useCheckboxes != 2 and ($useCheckboxes == 1 or formulizePermHandler::user_can_delete_entry($fid, $uid, $entry_id))) {
-                            $selectionCheckbox = "<input type=checkbox title='" . _formulize_DE_DELBOXDESC . "' class='formulize_selection_checkbox' name='delete_" . $entry_id . "' id='delete_" . $entry_id . "' value='delete_" . $entry_id . "'>";
-						} else {
-							$selectionCheckbox = "";
-						}
-					} // end of IF NO LOCKCONTROLS
-
-					$ids = internalRecordIds($entry, $mainFormHandle);
-					foreach($inlineButtons as $caid=>$thisCustomAction) {
-						list($caCode) = processCustomButton($caid, $thisCustomAction, $ids[0], $entry); // only bother with the code, since we already processed any clicked button above
-						if($caCode) {
-							${$thisCustomAction['handle']} = $caCode; // assign the button code that was returned
-						}
-					}
-					
-					// handle hidden elements for passing back to custom buttons
-					foreach($hiddenColumns as $thisHiddenCol) {
-						print "\n<input type=\"hidden\" name=\"hiddencolumn_".$linkids[0]."_$thisHiddenCol\" value=\"" . htmlspecialchars(display($entry, $thisHiddenCol)) . "\"></input>\n";
-					}
-					
-					include XOOPS_ROOT_PATH."/modules/formulize/templates/screens/default/".$screen->getVar('sid')."/listtemplate.php";
-				}
-			}
-		}
-
 	}// END OF MASTER HIDELIST CONDITIONAL
 	if((!isset($data) OR count($data) == $blankentries) AND !$LOE_limit) { // if no data was returned, or the dataset was empty...
 		print "<p><b>" . _formulize_DE_NODATAFOUND . "</b></p>\n";
