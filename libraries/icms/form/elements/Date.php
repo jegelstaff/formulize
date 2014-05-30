c01e52f997455ad151dcaa4b2b24960b0ce6682a
Daniel Ferguson <eikonos@gmail.com>
2014-05-30 14:35:04 -0700

    Mark the calendar icon so it does not print.

--- libraries/icms/form/elements/Date.php
+++ libraries/icms/form/elements/Date.php
@@ -73,11 +73,11 @@ class icms_form_elements_Date extends icms_form_elements_Text {
 
 		include_once ICMS_ROOT_PATH . '/include/calendar' . ($icmsConfigPersona['use_jsjalali'] == true ? 'jalali' : '') . 'js.php';
 
-		$result = "<input type='text' name='".$ele_name."' id='".$ele_name."' class=\"icms-date-box\" size='".$this->getSize()."' maxlength='".$this->getMaxlength()."' value='".$ele_value."'".$this->getExtra()." />&nbsp;&nbsp;<img src='" . ICMS_URL . "/images/calendar.png' alt='"._CALENDAR."' title='"._CALENDAR."' id='btn_".$ele_name."' onclick='return showCalendar(\"".$ele_name."\");'>";
+		$result = "<input type='text' name='".$ele_name."' id='".$ele_name."' class=\"icms-date-box\" size='".$this->getSize()."' maxlength='".$this->getMaxlength()."' value='".$ele_value."'".$this->getExtra()." />&nbsp;&nbsp;<img src='" . ICMS_URL . "/images/calendar.png' class='no-print' alt='"._CALENDAR."' title='"._CALENDAR."' id='btn_".$ele_name."' onclick='return showCalendar(\"".$ele_name."\");'>";
 
 		if ($icmsConfigPersona['use_jsjalali']) {
 			$dateFormat = dateFormatToStrftime(_SHORTDATESTRING);
-			$result = "<input name='".$ele_name."' id='".$ele_name."' class=\"icms-date-box\" size='".$this->getSize()."' maxlength='".$this->getMaxlength()."' value='".$jalali_ele_value."'".$this->getExtra()." />&nbsp;&nbsp;<img src='" . ICMS_URL . "/images/calendar.png' alt='"._CALENDAR."' title='"._CALENDAR."' id='btn_".$ele_name."'><script type='text/javascript'>
+			$result = "<input name='".$ele_name."' id='".$ele_name."' class=\"icms-date-box\" size='".$this->getSize()."' maxlength='".$this->getMaxlength()."' value='".$jalali_ele_value."'".$this->getExtra()." />&nbsp;&nbsp;<img src='" . ICMS_URL . "/images/calendar.png' class='no-print' alt='"._CALENDAR."' title='"._CALENDAR."' id='btn_".$ele_name."'><script type='text/javascript'>
 				Calendar.setup({
 					inputField  : '".$ele_name."',
 					ifFormat    : '$dateFormat',
