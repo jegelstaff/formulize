475285b7475193454d943e8a99c6a7aa1dc80bdb
Julian Egelstaff <nospam@yourturn.ca>
2014-08-27 12:51:48 -0400

    Fixing en/fr bug

--- language/english/xoopsmailerlocal.php
+++ language/english/xoopsmailerlocal.php
@@ -14,7 +14,7 @@ class XoopsMailerLocal extends icms_messaging_EmailHandler {
 	public function __construct() {
 		parent::__construct();
 		// You MUST specify the language code value so that the file exists: XOOPS_ROOT_PAT/class/mail/phpmailer/language/lang-["your-language-code"].php
-		$this->SetLanguage("fr");
+		$this->SetLanguage("en");
 	}
 
 	// Multibyte languages are encouraged to make their proper method for encoding FromName
