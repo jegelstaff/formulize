d0c1b02ee5e4b35cbf00dbd9194dd401b089ac3a
Julian Egelstaff <nospam@yourturn.ca>
2014-09-21 12:02:55 -0400

    trying full db update [update test db]

--- ci/travis/update-test-db.sh
+++ ci/travis/update-test-db.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-echo $TRAVIS_BRANCH
-echo $TRAVIS_COMMIT_MESSAGE
+echo 'Updating formulize_test_db.sql dump with current database'
 
-
-# - mysqldump -u travis formulize > ci/formulize_test_db.sql
-# - git config user.email "formulize@travis.ci"
-# - git config user.name "Travis CI"
-# - git config push.default simple
-# - git add ci/formulize_test_db.sql
-# - git commit -m "Travis updating test DB [skip ci]"
-# - stty -echo
-# - git push https://${GITHUB_TOKEN}@github.com/jegelstaff/formulize.git HEAD:${TRAVIS_BRANCH}
-# - stty echo
+mysqldump -u travis formulize > ci/formulize_test_db.sql
+git config user.email "formulize@travis.ci"
+git config user.name "Travis CI"
+git config push.default simple
+git add ci/formulize_test_db.sql
+git commit -m "Travis updating test DB [skip ci]"
+stty -echo
+git push https://${GITHUB_TOKEN}@github.com/jegelstaff/formulize.git HEAD:master
+stty echo
 
