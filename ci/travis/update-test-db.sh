085d0b274ed7f037e5117438b400680207c9d380
Daniel Ferguson <eikonos@gmail.com>
2014-09-22 21:11:03 -0700

    Only allow dumping the CI database when on the master branch. [skip ci]

--- ci/travis/update-test-db.sh
+++ ci/travis/update-test-db.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-echo 'Updating formulize_test_db.sql dump with current database'
+if [ "master" == "${TRAVIS_BRANCH}" ]; then
+    echo 'Updating formulize_test_db.sql dump with current database.'
 
-mysqldump -u travis formulize > ci/formulize_test_db.sql
-git config user.email "travis-ci@yourturn.ca"
-git config user.name "Travis CI"
-git config push.default simple
-git add ci/formulize_test_db.sql
-git commit -m "Travis updating test DB [skip ci]"
-git push https://${GITHUB_TOKEN}@github.com/jegelstaff/formulize.git HEAD:master --quiet
-
+    mysqldump -u travis formulize > ci/formulize_test_db.sql
+    git config user.email "travis-ci@yourturn.ca"
+    git config user.name "Travis CI"
+    git config push.default simple
+    git add ci/formulize_test_db.sql
+    git commit -m "Travis updating test DB [skip ci]"
+    git push https://${GITHUB_TOKEN}@github.com/jegelstaff/formulize.git HEAD:master --quiet
+else
+    echo 'Not Updating formulize_test_db.sql dump with current database because this is not the master branch.'
+fi
