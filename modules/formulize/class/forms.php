ab76d9ada642812841079e95f30661a74987006d
susannahkirby <suki@ibiblio.org>
2015-03-05 11:09:51 -0800

    Refactoring complete.

--- modules/formulize/class/forms.php
+++ modules/formulize/class/forms.php
@@ -1260,60 +1260,8 @@ class formulizeFormsHandler {
 	{
 		$listScreenHandler = xoops_getmodulehandler('listOfEntriesScreen', 'formulize');
 		$screen = $listScreenHandler->create();
+		$listScreenHandler->setDefaultListScreenVars($screen, $defaultFormScreenId, $newtitle, $newfid);
 
-		// === begin code under consideration ===
-		//$listScreenHandler->setDefaultListScreenVars($screen, $defaultFormScreenId, $newtitle, $newfid);
-		//
-		// when above code is uncommented and below code is commented, I get the following error:
-		//
-		// [Mon Mar 02 13:02:24.138492 2015] [:error] [pid 1037] [client ::1:57107]
-		// icms_core_Logger::handleError(2, Invalid argument supplied for foreach(),
-		// /Users/sukizana/Sites/formulize/modules/formulize/admin/save/application_menu_entries_save.php, 48);,
-		// referer: http://localhost/formulize/modules/formulize/admin/ui.php?page=application&aid=2&tab=forms
-		//
-		// View
-		$screen->setVar('defaultview', 'all');
-		$screen->setVar('usecurrentviewlist', _formulize_DE_CURRENT_VIEW);
-		$screen->setVar('limitviews', serialize(array(0 => 'allviews')));
-		$screen->setVar('useworkingmsg', 1);
-		$screen->setVar('usescrollbox', 1);
-		$screen->setVar('entriesperpage', 10);
-		$screen->setVar('viewentryscreen', $defaultFormScreenId);
-		// Headings
-		$screen->setVar('useheadings', 1);
-		$screen->setVar('repeatheaders', 5);
-		$screen->setVar('usesearchcalcmsgs', 1);
-		$screen->setVar('usesearch', 1);
-		$screen->setVar('columnwidth', 0);
-		$screen->setVar('textwidth', 35);
-		$screen->setVar('usecheckboxes', 0);
-		$screen->setVar('useviewentrylinks', 1);
-		$screen->setVar('desavetext', _formulize_SAVE);
-		// Buttons
-		$screen->setVar('useaddupdate', _formulize_DE_ADDENTRY);
-		$screen->setVar('useaddmultiple', _formulize_DE_ADD_MULTIPLE_ENTRY);
-		$screen->setVar('useaddproxy', _formulize_DE_PROXYENTRY);
-		$screen->setVar('useexport', _formulize_DE_EXPORT);
-		$screen->setVar('useimport', _formulize_DE_IMPORT);
-		$screen->setVar('usenotifications', _formulize_DE_NOTBUTTON);
-		$screen->setVar('usechangecols', _formulize_DE_CHANGECOLS);
-		$screen->setVar('usecalcs', _formulize_DE_CALCS);
-		$screen->setVar('useadvcalcs', _formulize_DE_ADVCALCS);
-		$screen->setVar('useexportcalcs', _formulize_DE_EXPORT_CALCS);
-		$screen->setVar('useadvsearch', '');
-		$screen->setVar('useclone', _formulize_DE_CLONESEL);
-		$screen->setVar('usedelete', _formulize_DE_DELETESEL);
-		$screen->setVar('useselectall', _formulize_DE_SELALL);
-		$screen->setVar('useclearall', _formulize_DE_CLEARALL);
-		$screen->setVar('usereset', _formulize_DE_RESETVIEW);
-		$screen->setVar('usesave', _formulize_DE_SAVE);
-		$screen->setVar('usedeleteview', _formulize_DE_DELETE);
-		$screen->setVar('title', "Entries in '$newtitle'");
-		$screen->setVar('fid', $newfid);
-		$screen->setVar('frid', 0);
-		$screen->setVar('type', 'listOfEntries');
-		$screen->setVar('useToken', 1);
-		// === end code under consideration ===
 		if (!$defaultListScreenId = $listScreenHandler->insert($screen)) {
 			print "Error: could not create default list screen";
 			return $defaultListScreenId;
