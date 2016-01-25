38b5f4d5af65e9877ceac28a765065e37c500037
Lee Glendenning <glen9190@mylaurier.ca>
2016-01-25 17:14:20 -0500

    Fix PDO define bug for connecting to DB

--- modules/formulize/class/PDO_Config.php
+++ modules/formulize/class/PDO_Config.php
@@ -10,7 +10,7 @@ defined('DB_USER') ? NULL : define('DB_USER', XOOPS_DB_USER);
 defined('DB_PASS') ? NULL : define('DB_PASS', XOOPS_DB_PASS);
 defined('DB_NAME') ? NULL : define('DB_NAME', XOOPS_DB_NAME);
 defined('Prefix')  ? NULL : define('Prefix',XOOPS_DB_PREFIX);
-defined('SID') 	   ? define('SID',$xoopsUser->getVar('uid')):NULL;
+defined('SID') 	   ? NULL : define('SID',$xoopsUser->getVar('uid'));
 defined ('MOD_ID') ? NULL :define ('MOD_ID',getFormulizeModId()) ;
 
 
