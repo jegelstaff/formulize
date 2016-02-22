751df77cb79ad18c73a15e00b86663e92e078508
Lee Glendenning <glen9190@mylaurier.ca>
2016-02-21 21:31:59 -0500

    Revert "Introduce save lock functionality"
    This reverts commit 6821228636e32df4b943b7668e92920ef3938dc8.

--- modules/formulize/admin/ui.php
+++ modules/formulize/admin/ui.php
@@ -37,39 +37,29 @@ include_once XOOPS_ROOT_PATH . "/modules/formulize/include/functions.php";
 
 global $xoopsTpl;
 
-// If saveLock is turned on, exit
-if(saveLock) {
-		exit();
-	}
-}
+if (!isset($xoopsTpl)) {
+    global $xoopsOption, $xoopsConfig, $xoopsModule;
 
+    $xoopsOption['theme_use_smarty'] = 1;
 
-if(!isset($xoopsTpl)) {
-	global $xoopsOption, $xoopsConfig, $xoopsModule;
+    // include Smarty template engine and initialize it
+    require_once XOOPS_ROOT_PATH . '/class/template.php';
+    require_once XOOPS_ROOT_PATH . '/class/theme.php';
+    require_once XOOPS_ROOT_PATH . '/class/theme_blocks.php';
 
-  $xoopsOption['theme_use_smarty'] = 1;
+    if ( @$xoopsOption['template_main'] ) {
+        if ( false === strpos( $xoopsOption['template_main'], ':' ) ) {
+            $xoopsOption['template_main'] = 'db:' . $xoopsOption['template_main'];
+        }
+    }
+    $xoopsThemeFactory = new xos_opal_ThemeFactory();
+    $xoopsThemeFactory->allowedThemes = $xoopsConfig['theme_set_allowed'];
+    $xoopsThemeFactory->defaultTheme = $xoopsConfig['theme_set'];
 
-  // include Smarty template engine and initialize it
-  require_once XOOPS_ROOT_PATH . '/class/template.php';
-  require_once XOOPS_ROOT_PATH . '/class/theme.php';
-  require_once XOOPS_ROOT_PATH . '/class/theme_blocks.php';
-
-	if ( @$xoopsOption['template_main'] ) {
-		if ( false === strpos( $xoopsOption['template_main'], ':' ) ) {
-			$xoopsOption['template_main'] = 'db:' . $xoopsOption['template_main'];
-		}
-	}
-	$xoopsThemeFactory = new xos_opal_ThemeFactory();
-  $xoopsThemeFactory->allowedThemes = $xoopsConfig['theme_set_allowed'];
-  $xoopsThemeFactory->defaultTheme = $xoopsConfig['theme_set'];
-
-  $xoTheme =& $xoopsThemeFactory->createInstance( array(
-  	'contentTemplate' => @$xoopsOption['template_main'],
-  ) );
-  $xoopsTpl =& $xoTheme->template;
-
-	//$xoTheme->addScript( '/include/xoops.js', array( 'type' => 'text/javascript' ) );
-	//$xoTheme->addScript( '/include/linkexternal.js', array( 'type' => 'text/javascript' ) );
+    $xoTheme =& $xoopsThemeFactory->createInstance(array(
+        'contentTemplate' => @$xoopsOption['template_main'],
+    ));
+    $xoopsTpl =& $xoTheme->template;
 }
 
 // handle any operations requested as part of this page load
@@ -79,57 +69,63 @@ include_once "op.php";
 // create the contents that we want to display for the currently selected page
 // the included php files create the values for $adminPage that are used for this page
 $adminPage = array();
-switch($_GET['page']) {
-  case "application":
-    include "application.php"; 
-    break;
-  case "form":
-    include "form.php";
-    break;
-	case "screen":
-		include "screen.php";
-		break;
-	case "relationship":
-		include "relationship.php";
-		break;
-	case "element":
-		include "element.php";
-		break;
-	case "advanced-calculation":
-		include "advanced_calculation.php";
-		break;
-	default:
-	case "home":
-		include "home.php";
-		break;
-
+$active_page = isset($_GET['page']) ? $_GET['page'] : "home";
+switch($active_page) {
+    case "application":
+        include "application.php";
+        break;
+    case "form":
+        include "form.php";
+        break;
+    case "screen":
+        include "screen.php";
+        break;
+    case "relationship":
+        include "relationship.php";
+        break;
+    case "element":
+        include "element.php";
+        break;
+    case "advanced-calculation":
+        include "advanced_calculation.php";
+        break;
+    case "synchronize":
+        include "synchronize.php";
+        break;
+    default:
+    case "home":
+        include "home.php";
+        break;
 }
+
 $adminPage['logo'] = "/modules/formulize/images/formulize-logo.png";
 
-
-
 // assign the default selected tab, if any:
-if(isset($_GET['tab']) AND (!isset($_POST['tabs_selected']) OR $_POST['tabs_selected'] === "")) {
-  foreach($adminPage['tabs'] as $selected=>$tabData) {
-    if(strtolower($tabData['name']) == $_GET['tab']) {
-      $adminPage['tabselected'] = $selected-1;
-      break;
+if (isset($_GET['tab']) AND (!isset($_POST['tabs_selected']) OR $_POST['tabs_selected'] === "")) {
+    foreach($adminPage['tabs'] as $selected=>$tabData) {
+        if (strtolower($tabData['name']) == $_GET['tab']) {
+            $adminPage['tabselected'] = $selected-1;
+            break;
+        }
     }
-  }
-} elseif($_POST['tabs_selected'] !== "") {
-	$adminPage['tabselected']  = intval($_POST['tabs_selected']);
+} elseif (isset($_POST['tabs_selected']) and $_POST['tabs_selected'] !== "") {
+    $adminPage['tabselected']  = intval($_POST['tabs_selected']);
 }
+
+// retrieve the xoops_version info
+$module_handler = xoops_gethandler('module');
+$formulizeModule = $module_handler->getByDirname("formulize");
+$metadata = $formulizeModule->getInfo();
 
 // assign the contents to the template and display
 $adminPage['formulizeModId'] = getFormulizeModId();
+$xoopsTpl->assign('version', $metadata['version']);
 $xoopsTpl->assign('adminPage', $adminPage);
-$xoopsTpl->assign('breadcrumbtrail', $breadcrumbtrail);
-$xoopsTpl->assign('scrollx', intval($_POST['scrollx']));
+if (isset($breadcrumbtrail))
+    $xoopsTpl->assign('breadcrumbtrail', $breadcrumbtrail);
+$xoopsTpl->assign('scrollx', (isset($_POST['scrollx']) ? intval($_POST['scrollx']) : 0));
 $accordion_active = (isset($_POST['accordion_active']) AND $_POST['accordion_active'] !== "" AND $_POST['accordion_active'] !== "false") ? intval($_POST['accordion_active']) : "false";
 $xoopsTpl->assign('accordion_active', $accordion_active);
 $xoopsTpl->display("db:admin/ui.html");
 
-include 'footer.php';
 xoops_cp_footer();
-
-
