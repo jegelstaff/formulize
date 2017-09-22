97044105baadd7fdcb5f6bcf5282515702c85858
lelbarton <lauraelbarton@gmail.com>
2017-09-22 14:32:19 -0400

    Display correct success or failure icon beside each installation requirement
    The image yes.png was displayed for all requirements regardless of whether they had been met or not. This implements conditionals to display no.png if there is a problem.

--- install/page_modcheck.php
+++ install/page_modcheck.php
@@ -26,124 +26,140 @@ $pageHasForm = false;
 $diagsOK = false;
 
 function xoDiag( $status = -1, $str = '') {
-	if ($status == -1) {
-		$GLOBALS['error'] = true;
-	}
-	$classes = array( -1 => 'error', 0 => 'warning', 1 => 'success' );
-	$strings = array( -1 => FAILED, 0 => WARNING, 1 => SUCCESS );
-	if (empty($str)) {
-		$str = $strings[$status];
-	}
-	return '<td class="' . $classes[$status] . '">' . $str . '</td>';
+    if ($status == -1) {
+        $GLOBALS['error'] = true;
+    }
+    $classes = array( -1 => 'error', 0 => 'warning', 1 => 'success' );
+    $strings = array( -1 => FAILED, 0 => WARNING, 1 => SUCCESS );
+    if (empty($str)) {
+        $str = $strings[$status];
+    }
+    return '<td class="' . $classes[$status] . '">' . $str . '</td>';
 }
+
 function xoDiagBoolSetting( $name, $wanted = false, $severe = false) {
-	$setting = strtolower( ini_get( $name ) );
-	$setting = ( empty( $setting ) || $setting == 'off' || $setting == 'false' ) ? false : true;
-	if ($setting == $wanted) {
-		return xoDiag( 1, $setting ? 'ON' : 'OFF' );
-	} else {
-		return xoDiag( $severe ? -1 : 0, $setting ? 'ON' : 'OFF' );
-	}
+    $setting = strtolower( ini_get( $name ) );
+    $setting = ( empty( $setting ) || $setting == 'off' || $setting == 'false' ) ? false : true;
+    if ($setting == $wanted) {
+        return xoDiag( 1, $setting ? 'ON' : 'OFF' ) . successImg(1);
+    } else {
+        $severity = $severe ? -1 : 0;
+        return xoDiag( $severity, $setting ? 'ON' : 'OFF' ) . successImg($severity);
+    }
 }
 
 function xoDiagIfWritable( $path) {
-	$path = "../" . $path;
-	$error = true;
-	if (!is_dir( $path )) {
-		if (file_exists( $path )) {
-			@chmod( $path, 0666 );
-			$error = !is_writeable( $path );
-		}
-	} else {
-		@chmod( $path, 0777 );
-		$error = !is_writeable( $path );
-	}
-	return xoDiag( $error ? -1 : 1, $error ? 'Not writable' : 'Writable' );
+    $path = "../" . $path;
+    $error = true;
+    if (!is_dir( $path )) {
+        if (file_exists( $path )) {
+            @chmod( $path, 0666 );
+            $error = !is_writeable( $path );
+        }
+    } else {
+        @chmod( $path, 0777 );
+        $error = !is_writeable( $path );
+    }
+    return xoDiag( $error ? -1 : 1, $error ? 'Not writable' : 'Writable' );
 }
+
+function phpVersionCheck() {
+    if (version_compare( phpversion(), '5.7', '>=')) {
+        return 1;
+    } elseif (version_compare( phpversion(), '5.6', '>=')) {
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+function successImg($success_val) {
+    $img_name = $success_val >= 0 ? "yes" : "no";
+    return '<img src="img/' . $img_name . '.png" alt="Success?" class="rootimg" />';
+}
+
+$php_version_success = phpVersionCheck();
+$mysql_success = function_exists('mysql_connect') ? 1 : -1;
+$extension_success = extension_loaded('session') ? 1 : -1;
+$pcre_success = extension_loaded('pcre') ? 1 : -1;
+
+$php_version_success_str = xoDiag($php_version_success, phpversion()) . successImg($php_version_success);
+$mysql_success_str = xoDiag($mysql_success) . successImg($mysql_success);
+$extension_success_str = xoDiag($extension_success) . successImg($extension_success);
+$pcre_success_str = xoDiag($pcre_success) . successImg($pcre_success);
 
 ob_start();
 ?>
-<fieldset>
-<h3><?php echo REQUIREMENTS; ?></h3>
-<h4><?php echo SERVER_API; ?>:&nbsp; <?php echo php_sapi_name(); ?> <img
-	src="img/yes.png" alt="Success" class="rootimg" /></h4>
-<div class="clear">&nbsp;</div>
-<h4><?php echo _PHP_VERSION; ?>:&nbsp; <?php
-if (version_compare( phpversion(), '5.2', '>=')) {
-	echo xoDiag( 1, phpversion() );
-} elseif (version_compare( phpversion(), '5.1', '>=')) {
-	echo xoDiag( 0, phpversion() );
-} else {
-	echo xoDiag( -1, phpversion() );
-}
-?> <img
-	src="img/<?php echo (isset($php_version_error) ? "no" : "yes") ?>.png"
-	alt="Success" class="rootimg" /></h4>
-<div class="clear">&nbsp;</div>
-<h4><?php printf( PHP_EXTENSION, 'MySQL' ); ?>:&nbsp; <?php echo xoDiag( function_exists( 'mysql_connect' ) ? 1 : -1 ); ?>
-<img src="img/yes.png" alt="Success" class="rootimg" /></h4>
-<div class="clear">&nbsp;</div>
-<h4><?php printf( PHP_EXTENSION, 'Session' ); ?>:&nbsp; <?php echo xoDiag( extension_loaded( 'session' ) ? 1 : -1 ); ?>
-<img src="img/yes.png" alt="Success" class="rootimg" /></h4>
-<div class="clear">&nbsp;</div>
-<h4><?php printf( PHP_EXTENSION, 'PCRE' ); ?>:&nbsp; <?php echo xoDiag( extension_loaded( 'pcre' ) ? 1 : -1 ); ?>
-<img src="img/yes.png" alt="Success" class="rootimg" /></h4>
-<div class="clear">&nbsp;</div>
-<h4>file_uploads:&nbsp; <?php echo xoDiagBoolSetting( 'file_uploads', true ); ?>
-<img src="img/yes.png" alt="Success" class="rootimg" /></h4>
-<div class="clear">&nbsp;</div>
-</fieldset>
 
-<fieldset>
-<h3><?php echo RECOMMENDED_EXTENSIONS; ?></h3>
-<p><?php echo RECOMMENDED_EXTENSIONS_MSG; ?></p>
-<div class="clear">&nbsp;</div>
+    <fieldset>
+        <h3><?php echo REQUIREMENTS; ?></h3>
+        <h4><?php echo SERVER_API; ?>:&nbsp; <?php echo php_sapi_name(); ?> <img
+                    src="img/yes.png" alt="Success" class="rootimg" /></h4>
+        <div class="clear">&nbsp;</div>
+        <h4><?php echo _PHP_VERSION; ?>:&nbsp;
+            <?php echo $php_version_success_str ?>
+        </h4>
+        <div class="clear">&nbsp;</div>
+        <h4><?php printf( PHP_EXTENSION, 'MySQL' ); ?>:&nbsp; <?php echo $mysql_success_str; ?> </h4>
+        <div class="clear">&nbsp;</div>
+        <h4><?php printf( PHP_EXTENSION, 'Session' ); ?>:&nbsp; <?php echo $extension_success_str ?></h4>
+        <div class="clear">&nbsp;</div>
+        <h4><?php printf( PHP_EXTENSION, 'PCRE' ); ?>:&nbsp; <?php echo $pcre_success_str ?></h4>
+        <div class="clear">&nbsp;</div>
+        <h4>file_uploads:&nbsp; <?php echo xoDiagBoolSetting( 'file_uploads', true ); ?>
+        <div class="clear">&nbsp;</div>
+    </fieldset>
 
-<h4><?php printf( PHP_EXTENSION, CHAR_ENCODING ); ?>:&nbsp; <?php
-$ext = array();
-if (extension_loaded( 'iconv' ) )		$ext[] = 'Iconv';
-if (extension_loaded( 'mb_string' ) )	$ext[] = 'MBString';
-if (empty($ext)) {
-	echo xoDiag( 0, NONE );
-} else {
-	echo xoDiag( 1, implode( ',', $ext ) );
-}
-?> <img src="img/yes.png" alt="Success" class="rootimg" /></h4>
-<div class="clear">&nbsp;</div>
-<h4><?php printf( PHP_EXTENSION, XML_PARSING ); ?>:&nbsp; <?php
-$ext = array();
-if (extension_loaded( 'xml' ) )		$ext[] = 'XML';
-//if (extension_loaded( 'dom' ) )		$ext[] = 'DOM';
-if (empty($ext)) {
-	echo xoDiag( 0, NONE );
-} else {
-	echo xoDiag( 1, implode( ',', $ext ) );
-}
-?> <img src="img/yes.png" alt="Success" class="rootimg" /></h4>
-<div class="clear">&nbsp;</div>
-<h4><?php printf( PHP_EXTENSION, OPEN_ID ); ?>:&nbsp; <?php
-$ext = array();
-if (extension_loaded( 'curl' ) )		$ext[] = 'Curl  <img src="img/yes.png" alt="Success" class="rootimg" />  ';
-if (extension_loaded( 'bcmath' ) )		$ext[] = ' Math Support  <img src="img/yes.png" alt="Success" class="rootimg" />  ';
-if (extension_loaded( 'openssl' ) )	$ext[] = ' OpenSSL  <img src="img/yes.png" alt="Success" class="rootimg" />';
-if (empty($ext)) {
-	echo xoDiag( 0, NONE );
-} else {
-	echo xoDiag( 1, implode( ' ', $ext ) );
-}
-?></h4>
-<div class="clear">&nbsp;</div>
-</fieldset>
-<!--
+    <fieldset>
+        <h3><?php echo RECOMMENDED_EXTENSIONS; ?></h3>
+        <p><?php echo RECOMMENDED_EXTENSIONS_MSG; ?></p>
+        <div class="clear">&nbsp;</div>
+
+        <h4><?php printf( PHP_EXTENSION, CHAR_ENCODING ); ?>:&nbsp; <?php
+            $ext = array();
+            if (extension_loaded( 'iconv' ) )		$ext[] = 'Iconv';
+            if (extension_loaded( 'mb_string' ) )	$ext[] = 'MBString';
+            if (empty($ext)) {
+                echo xoDiag( 0, NONE );
+            } else {
+                echo xoDiag( 1, implode( ',', $ext ) );
+            }
+            ?> <img src="img/yes.png" alt="Success" class="rootimg" /></h4>
+        <div class="clear">&nbsp;</div>
+        <h4><?php printf( PHP_EXTENSION, XML_PARSING ); ?>:&nbsp; <?php
+            $ext = array();
+            if (extension_loaded( 'xml' ) )		$ext[] = 'XML';
+            //if (extension_loaded( 'dom' ) )		$ext[] = 'DOM';
+            if (empty($ext)) {
+                echo xoDiag( 0, NONE );
+            } else {
+                echo xoDiag( 1, implode( ',', $ext ) );
+            }
+            ?> <img src="img/yes.png" alt="Success" class="rootimg" /></h4>
+        <div class="clear">&nbsp;</div>
+        <h4><?php printf( PHP_EXTENSION, OPEN_ID ); ?>:&nbsp; <?php
+            $ext = array();
+            if (extension_loaded( 'curl' ) )		$ext[] = 'Curl  <img src="img/yes.png" alt="Success" class="rootimg" />  ';
+            if (extension_loaded( 'bcmath' ) )		$ext[] = ' Math Support  <img src="img/yes.png" alt="Success" class="rootimg" />  ';
+            if (extension_loaded( 'openssl' ) )	$ext[] = ' OpenSSL  <img src="img/yes.png" alt="Success" class="rootimg" />';
+            if (empty($ext)) {
+                echo xoDiag( 0, NONE );
+            } else {
+                echo xoDiag( 1, implode( ' ', $ext ) );
+            }
+            ?></h4>
+        <div class="clear">&nbsp;</div>
+    </fieldset>
+    <!--
 	<table class="diags">
 	<caption><?php echo FILE_PERMISSIONS; ?></caption>
     <thead>
     	<tr><th>Path</th><th>Status</th></tr>
     </thead>
 	<?php
-		$paths = array("uploads/", "cache/", "templates_c/", "mainfile.php");
-		foreach ( $paths as $path) {
-	?>
+    $paths = array("uploads/", "cache/", "templates_c/", "mainfile.php");
+    foreach ( $paths as $path) {
+        ?>
 	<tr>
 		<th scope="row"><?php echo $path; ?></th>
 		<td><?php echo xoDiagIfWritable( $path ); ?></td>
@@ -151,10 +167,10 @@ if (empty($ext)) {
 	<?php } ?>
 	</table>
 	-->
-	<?php
-	$content = ob_get_contents();
-	ob_end_clean();
+<?php
+$content = ob_get_contents();
+ob_end_clean();
 
-	include 'install_tpl.php';
+include 'install_tpl.php';
 
-	?>
+?>
