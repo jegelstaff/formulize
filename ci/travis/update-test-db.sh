1f42f863a9f986ff3cc2d5be9a4dcea73c685512
Julian Egelstaff <nospam@yourturn.ca>
2018-01-20 11:29:54 -0500

    sudo [update test db]

--- ci/travis/update-test-db.sh
+++ ci/travis/update-test-db.sh
@@ -2,12 +2,12 @@
 
 if [ "master" == "${TRAVIS_BRANCH}" -a "false" == "${TRAVIS_PULL_REQUEST}" ]; then
     echo 'Updating formulize_test_db.sql dump with current database.'
-    mkdir /var/www/master/
+    sudo mkdir /var/www/master/
     sudo chmod 777 /var/www/master/
     cd /var/www/master/
-    git clone https://github.com/jegelstaff/formulize.git
-    sudo chmod 777 /var/www/master/ci/formulize_test_db.sql
-    mysqldump -u travis formulize > /var/www/master/ci/formulize_test_db.sql
+    sudo git clone https://github.com/jegelstaff/formulize.git
+    sudo chmod 777 /var/www/master/formulize/ci/formulize_test_db.sql
+    mysqldump -u travis formulize > /var/www/master/formulize/ci/formulize_test_db.sql
     git config user.email "travis-ci@yourturn.ca"
     git config user.name "Travis CI"
     git config push.default simple
