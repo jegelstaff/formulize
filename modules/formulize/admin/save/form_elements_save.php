3c97968714ccb3c1ceb6530689b91c16d4dbfd7e
Julian Egelstaff <nospam@yourturn.ca>
2018-04-02 18:06:52 -0400

    Don't make data fields for non db cloned elements

--- modules/formulize/admin/save/form_elements_save.php
+++ modules/formulize/admin/save/form_elements_save.php
@@ -205,14 +205,18 @@ if($_POST['cloneelement']) {
   }
   $element_handler->insert($thisElementObject);
   $ele_id = $thisElementObject->getVar('ele_id');
-  $fieldStateSQL = "SHOW COLUMNS FROM " . $xoopsDB->prefix("formulize_" . $thisElementObject->getVar('form_handle')) ." LIKE '$oldHandle'"; // note very odd use of LIKE as a clause of its own in SHOW statements, very strange, but that's what MySQL does
-  	if(!$fieldStateRes = $xoopsDB->query($fieldStateSQL)) {
-		$dataType = "text";
-	} else {
-		$fieldStateData = $xoopsDB->fetchArray($fieldStateRes);
-		$dataType = $fieldStateData['Type'];
-	}
-  $form_handler->insertElementField($thisElementObject, $dataType); 
+  $ele_type = $thisElementObject->getVar('ele_type');
+  $databaseElement = ($ele_type == "areamodif" OR $ele_type == "ib" OR $ele_type == "sep" OR $ele_type == "subform" OR $ele_type == "grid" OR (property_exists($thisElementObject,'hasData') AND $thisElementObject->hasData == false) ) ? false : true;
+  if($databaseElement) {
+    $fieldStateSQL = "SHOW COLUMNS FROM " . $xoopsDB->prefix("formulize_" . $thisElementObject->getVar('form_handle')) ." LIKE '$oldHandle'"; // note very odd use of LIKE as a clause of its own in SHOW statements, very strange, but that's what MySQL does
+      if(!$fieldStateRes = $xoopsDB->query($fieldStateSQL)) {
+          $dataType = "text";
+      } else {
+          $fieldStateData = $xoopsDB->fetchArray($fieldStateRes);
+          $dataType = $fieldStateData['Type'];
+      }
+    $form_handler->insertElementField($thisElementObject, $dataType);
+  }
   print "/* eval */ window.location = '".XOOPS_URL."/modules/formulize/admin/ui.php?page=element&ele_id=$ele_id&aid=".intval($_POST['aid'])."';";
 }
 
