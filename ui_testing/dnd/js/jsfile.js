2a2bf2e72d35d6b7c30f5b2f16abe745a658ea2f
GelScott <angele.lys@hotmail.com>
2018-10-19 20:14:51 -0400

    initial dropping functionality

--- ui_testing/dnd/js/jsfile.js
+++ ui_testing/dnd/js/jsfile.js
@@ -1,15 +1,12 @@
-$(document).ready(function(){
-$( ".form-header" ).draggable({    
-    stack: ".form",
-    revert : function(event, ui) {
-            //return to original position after dropping
-        $(this).data("uiDraggable").originalPosition = {
-                top : 0,
-                left : 0
-            };
-            return !event;
-        
-    }   
-    
+$(document).ready(function () {
+    $(".form-draggable").draggable({
+        stack: '.form-draggable', 
+        connectToSortable: '.form-content',
+        containment:'.form-container',
+        helper:'clone',        
+    });
+    $(".form-content").sortable();
 });
-});
+
+
+
