36f5a385e5736c997f89c572be82b10262b3f4b0
GelScott <angele.lys@hotmail.com>
2018-10-20 17:52:40 -0400

    form interactions and popup menu

--- ui_testing/dnd/js/jsfile.js
+++ ui_testing/dnd/js/jsfile.js
@@ -1,12 +1,38 @@
 $(document).ready(function () {
-    $(".form-draggable").draggable({
-        stack: '.form-draggable', 
-        connectToSortable: '.form-content',
-        containment:'.form-container',
-        helper:'clone',        
-    });
-    $(".form-content").sortable();
+	$(".form-draggable").draggable({
+		stack: '.form-draggable',
+		helper: 'clone',
+	});
+	/*
+	$(".addable").draggable({
+		stack: '.form-draggable',
+		connectToSortable: '.form-content',
+		containment: '.form-container',
+		helper: 'clone',
+		stop: function () {
+			// When dragging stops, revert the draggable to its
+			// original starting position.
+			$(".form-draggable").removeClass("addable");
+		},
+	});
+	*/
+	$(".form-content").droppable({
+    accept: ".addable",
+    hoverClass: "form-focus",
+    drop: function(ev, ui) {
+        //ui.draggable.remove();
+        var item = ui.draggable.clone();
+        item.appendTo($(this));
+		item.removeClass("addable");
+		item.removeClass("tilt");
+		item.attr("style", "");
+		item.draggable({
+			stack: '.form-draggable',
+			helper: 'clone',
+		});
+    }
+	});
+	
+	$(".form-content").sortable();
 });
-
-
 
