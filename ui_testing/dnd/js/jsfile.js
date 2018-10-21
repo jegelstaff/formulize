4d863e9c2dd707b1c2292d93a4dc1e8fa54b2776
GelScott <angele.lys@hotmail.com>
2018-10-21 17:55:50 -0400

    form drag effects

--- ui_testing/dnd/js/jsfile.js
+++ ui_testing/dnd/js/jsfile.js
@@ -1,38 +1,51 @@
 $(document).ready(function () {
+	/*enable removal for current relationships (incomplete)*/
 	$(".form-draggable").draggable({
 		stack: '.form-draggable',
 		helper: 'clone',
-	});
-	/*
-	$(".addable").draggable({
-		stack: '.form-draggable',
-		connectToSortable: '.form-content',
-		containment: '.form-container',
-		helper: 'clone',
-		stop: function () {
-			// When dragging stops, revert the draggable to its
-			// original starting position.
-			$(".form-draggable").removeClass("addable");
+		revert: function (event, ui) {
+			/*Animate form release*/
+			$(this).data("uiDraggable").originalPosition = {
+				top: $(this).position().top,
+				left: $(this).position().left
+			};
+			return !event;
+		},
+		/*tilt effect on drag*/
+		start: function (event, ui) {
+			$(ui.helper).addClass("form-draggable-helper");
 		},
 	});
-	*/
+
+	/*Create a new relationship with a form*/
 	$(".form-content").droppable({
-    accept: ".addable",
-    hoverClass: "form-focus",
-    drop: function(ev, ui) {
-        //ui.draggable.remove();
-        var item = ui.draggable.clone();
-        item.appendTo($(this));
-		item.removeClass("addable");
-		item.removeClass("tilt");
-		item.attr("style", "");
-		item.draggable({
-			stack: '.form-draggable',
-			helper: 'clone',
-		});
-    }
+		accept: ".addable",
+		hoverClass: "form-focus",
+		drop: function (ev, ui) {
+			//ui.draggable.remove();
+			var item = ui.draggable.clone();
+			item.appendTo($(this));
+			item.removeClass("addable");
+			item.removeClass("tilt");
+			item.attr("style", "");
+			item.draggable({
+				//Todo: fix duplicate code
+				stack: '.form-draggable',
+				helper: 'clone',
+				revert: function (event, ui) { /*Animate form release*/
+					$(this).data("uiDraggable").originalPosition = {
+						top: $(this).position().top,
+						left: $(this).position().left
+					};
+					return !event;
+				},
+				start: function (event, ui) {
+					$(ui.helper).addClass("form-draggable-helper");
+				},
+			});
+			$("#dialog").dialog("open");
+		}
 	});
-	
+
 	$(".form-content").sortable();
 });
-
