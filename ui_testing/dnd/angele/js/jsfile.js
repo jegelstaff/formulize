1887d9943e16daf85686ee5052cae040de1b259a
GelScott <angele.lys@hotmail.com>
2018-11-23 12:18:35 -0500

    initial treeview implementation

--- ui_testing/dnd/angele/js/jsfile.js
+++ ui_testing/dnd/angele/js/jsfile.js
@@ -1,86 +1,37 @@
 $(document).ready(function () {
-	/*enable removal for current relationships (incomplete)*/
-	$(".form-draggable").draggable({
-		stack: '.form-draggable',
-		helper: 'clone',
-		appendTo: 'body',
-		revert: 'invalid',
-		start: function (event, ui) {
-			/*tilt effect on drag*/
-			
-			$(ui.helper).addClass("form-draggable-helper");
-			$(ui.helper).css("width", $(this).width());
-			var dropContainer = $(".form-content");
-			dropContainer.animate({
-				height: dropContainer.height() + ui.helper.height()
-			}, 250);
-			dropContainer.addClass("form-focus");
-
-		},
-		stop: function (event, ui) {
-			$(".form-content").animate({
-				height: "0"
-			}, 200);
-			$(".form-content").removeClass("form-focus");
-
-			//$("#popup").css("display", "block");
+	
+	var $container = "<div class='drop'>xxx</div>";
+	
+	//Create forms
+	for (i = 0; i < 2; i++) { //for each individual relationship
+		var $formrel = [];
+		for (j = 0; j < 3; j++) {
+			var $append;
+			var $html;
+			if (j == 0) {
+				$append = '<i class="fas fa-file-alt">&nbsp<b>Form'+i+'</b></i>';
+				$html = $('<ul id="#form-listings">').append($append);
+			} else {
+				$append = '<i class="fas">&nbspForm2</i>';
+				$html = $('<ul id="#form-listings"><li><span class="list">').append($append).append('<ul class="hidelist"><li>├ Form2</li><li>└ Form3</li></ul></li></ul>');
+			}
+			//todo: add drop container (after form3)
+			$formrel[j] = $html;
 		}
-	});
+		$formrel[3 + 1]= $container;
 
-	/*Create a new relationship with a form*/
-	$(".form-content").droppable({
-		accept: ".addable",
-		drop: function (ev, ui) {
-			//ui.draggable.remove();
-			var item = ui.draggable.clone();
-			item.appendTo($(this));
-			item.removeClass("addable");
-			item.removeClass("tilt");
-			item.attr("style", "");
-			item.sortable({
-				//Todo: fix duplicate code
-				stack: '.form-draggable',
-				helper: 'clone',
-				revert: 'invalid',
-				axis: 'y',
-				containment: 'parent',
-				animation: 200,
-				start: function (event, ui) {
-					$(ui.helper).addClass("form-draggable-helper");
+		$('<div class="parent">').append($formrel).append($container).appendTo($('#form-container'));
+					//todo: add drop container (after last formrel)
+		
 
-					$(ui.helper).css("width", $(this).width());
-				},
-			});		
-			var type = "none";
-			if ($(this).hasClass('onemany')) {
-				type = "One to many"
-				item.append(relationshipDropDiv);
-            }
-            else if ($(this).hasClass('oneone')) {
-				type = "One to one";			item.append(relationshipDropDiv);
-            }
-			$("#relationshiptype").text(type);
-			$(this).animate({
-				height: "0"
-			}, 200);
-		}
-	});
-	$(".form-content").sortable({
-		placeholder: 'sortable-placeholder',
-		revert: 'invalid',
-	});
-
-	//basic popup functionality
-	var popup = document.getElementById('popup');
-	var span = document.getElementsByClassName("close")[0];
-	span.onclick = function () {
-		popup.style.display = "none";
-	}
-	window.onclick = function (event) {
-		if (event.target == popup) {
-			popup.style.display = "none";
-		}
 	}
 
-	var relationshipDropDiv = ("<div class='form-content' id='droppable'></div>");
+	var toggler = document.getElementsByClassName("list");
+	var i;
+
+	for (i = 0; i < toggler.length; i++) {
+		toggler[i].addEventListener("click", function () {			this.parentElement.parentElement.querySelector(".hidelist").classList.toggle("active");
+			this.classList.toggle("list-down");
+		});
+	}
 });
--- ui_testing/dnd/js/jsfile.js
+++ ui_testing/dnd/js/jsfile.js
@@ -1,103 +1,51 @@
-$(document).ready(function () {
-	/*enable removal for current relationships (incomplete)*/
-	$(".form-draggable").draggable({
-		stack: '.form-draggable',
-		helper: 'clone',
-		appendTo: 'body',
-		revert: 'invalid',
-		start: function (event, ui) {
-			/*tilt effect on drag*/
-			$(ui.helper).addClass("form-draggable-helper");
-			$(ui.helper).css("width", $(this).width());
-			var dropContainer = $(".form-content");
-			dropContainer.animate({
-				height: dropContainer.height() + ui.helper.height()
-			}, 250);
-			dropContainer.addClass("form-focus");
+onload = function () {
 
-		},
-		stop: function (event, ui) {
-			$(".form-content").animate({
-				height: "0"
-			}, 200);
-			$(".form-content").removeClass("form-focus");
+    // TreeView data
+    var items = [
+        { header: 'Electronics', img: 'resources/electronics.png', items: [
+            { header: 'Trimmers/Shavers' },
+            { header: 'Tablets' },
+            { header: 'Phones', img: 'resources/phones.png', items: [
+                { header: 'Apple' },
+                { header: 'Motorola' },
+                { header: 'Nokia' },
+                { header: 'Samsung' }
+            ]},
+            { header: 'Speakers' },
+            { header: 'Monitors' }
+        ]},
+        { header: 'Toys', img: 'resources/toys.png', items: [
+            { header: 'Shopkins' },
+        ]},
+        { header: 'Home', img: 'resources/home.png', items: [
+            { header: 'Coffeee Maker' },
+        ]}
+    ];
 
-<<<<<<< HEAD
-			//$("#popup").css("display", "block");
-=======
-			$("#popup").css("display", "block");
->>>>>>> c3e2d19f2a044d094544f288a7976c7fb6b0c1e7
-		}
-	});
+    // create and bind the TreeView
+    var tv = new wijmo.nav.TreeView('#tv', {
+        displayMemberPath: 'header',
+        childItemsPath: 'items',
+        itemsSource: items
+    });
 
-	/*Create a new relationship with a form*/
-	$(".form-content").droppable({
-		accept: ".addable",
-		drop: function (ev, ui) {
-			//ui.draggable.remove();
-			var item = ui.draggable.clone();
-			item.appendTo($(this));
-			item.removeClass("addable");
-			item.removeClass("tilt");
-			item.attr("style", "");
-			item.sortable({
-				//Todo: fix duplicate code
-				stack: '.form-draggable',
-				helper: 'clone',
-				revert: 'invalid',
-				axis: 'y',
-				containment: 'parent',
-				animation: 200,
-				start: function (event, ui) {
-					$(ui.helper).addClass("form-draggable-helper");
+    // handle collapse/expand buttons
+    document.getElementById('btnCollapse').addEventListener('click', function () {
+        tv.collapseToLevel(0);
+    });
+    document.getElementById('btnExpand').addEventListener('click', function () {
+        tv.collapseToLevel(1000);
+    });
 
-					$(ui.helper).css("width", $(this).width());
-				},
-			});		
-			var type = "none";
-<<<<<<< HEAD
-			if ($(this).hasClass('onemany')) {
-				type = "One to many"
-				item.append(relationshipDropDiv);
-            }
-            else if ($(this).hasClass('oneone')) {
-				type = "One to one";			item.append(relationshipDropDiv);
-=======
-			if ($(this).hasClass('manymany')){
-				type = "Many to many"
-            }
-            else if ($(this).hasClass('onemany')) {
-				type = "One to many"
-            }
-            else if ($(this).hasClass('oneone')) {
-				type = "One to one"
->>>>>>> c3e2d19f2a044d094544f288a7976c7fb6b0c1e7
-            }
-			$("#relationshiptype").text(type);
-			$(this).animate({
-				height: "0"
-			}, 200);
-		}
-	});
-	$(".form-content").sortable({
-		placeholder: 'sortable-placeholder',
-		revert: 'invalid',
-	});
-
-	//basic popup functionality
-	var popup = document.getElementById('popup');
-	var span = document.getElementsByClassName("close")[0];
-	span.onclick = function () {
-		popup.style.display = "none";
-	}
-	window.onclick = function (event) {
-		if (event.target == popup) {
-			popup.style.display = "none";
-		}
-	}
-
-<<<<<<< HEAD
-	var relationshipDropDiv = ("<div class='form-content' id='droppable'></div>");
-=======
->>>>>>> c3e2d19f2a044d094544f288a7976c7fb6b0c1e7
-});
+    // handle checkboxes
+    document.getElementById('chkAutoCollapse').addEventListener('change', function (e) {
+        tv.autoCollapse = e.target.checked;
+    });
+    document.getElementById('chkIsAnimated').addEventListener('change', function (e) {
+        tv.isAnimated = e.target.checked;
+    });
+    document.getElementById('chkExpandOnClick').addEventListener('change', function (e) {
+        tv.expandOnClick = e.target.checked;
+    });
+}
+                   
