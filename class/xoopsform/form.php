7ec3676bf3094ea5f98f28312d68c32b6c2f1d18
Julian Egelstaff <nospam@yourturn.ca>
2019-01-17 11:56:14 -0500

    Fixing constructors

--- class/xoopsform/form.php
+++ class/xoopsform/form.php
@@ -27,10 +27,6 @@
  */
 abstract class XoopsForm extends icms_form_Base {
 	private $_deprecated;
-	
-	public function XoopsForm($title, $name, $action, $method = "post", $addtoken = false) {
-		self::__construct($title, $name, $action, $method, $addtoken);
-	}
 	public function __construct($title, $name, $action, $method = "post", $addtoken = false) {
 		parent::__construct($title, $name, $action, $method, $addtoken);
 		$this->_deprecated = icms_core_Debug::setDeprecated('icms_form_elements_Button', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
