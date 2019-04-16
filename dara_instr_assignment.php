f67592a2bb0b8556f22cc6964d8e2827a3cfb0b6
Julian Egelstaff <nospam@yourturn.ca>
2019-04-16 08:56:43 -0400

    returning coordinators to the async assignment

--- dara_instr_assignment.php
+++ dara_instr_assignment.php
@@ -3,4 +3,4 @@
 // this file answers assignments made in the Director's module
 include_once 'mainfile.php';
 include_once XOOPS_ROOT_PATH.'/modules/formulize/include/readelements.php';
-print json_encode(array('instructors'=>$dara_instructorLoadUpdated, 'newEntryIds'=>$formulize_newEntryIds[15], 'deleteEntryIds'=>$dara_blankAssignmentsCleared));
+print json_encode(array('instructors'=>$dara_instructorLoadUpdated, 'newEntryIds'=>$formulize_newEntryIds[15], 'deleteEntryIds'=>$dara_blankAssignmentsCleared, 'coordinators'=>$dara_coordinators));
