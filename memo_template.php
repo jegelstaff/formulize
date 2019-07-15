041872a9cf90211c581f6e1c7cd22fced2874eb1
Julian Egelstaff <nospam@yourturn.ca>
2019-07-15 10:53:25 -0400

    Template (missed in prior commit)

--- memo_template.php
+++ memo_template.php
@@ -99,7 +99,8 @@ if(count($courses)>0 OR count($coordCourses)>0) {
                 $compLabelStart = ($compareOn AND !isset($lockSectionsByInst[$name][$sectionId])) ? "<span style=\"color: red;\">NEW: " : "";
                 $compLabelEnd = $compLabelStart ? "</span>" : "";
                 $courseLabel = "{$course['code']}, {$course['section']}{$course['reserved']}: {$course['title']}";
-                $timeAndRoom = "<BR>{$course['times']}, {$course['room']}";
+                $timeAndRoom = "<BR>{$course['times']}";
+                $timeAndRoom .= (isset($_POST['showrooms']) AND $_POST['showrooms']) ? ", {$course['room']}" : "";
                 $coTeaching = is_array($course['coinst']) > 0 ? '<BR>Co-taught with: '.implode(', ',$course['coinst']) : '';
                 $html .= "<TR><TD style=\"border-left: 1px solid black;border-right: 1px solid black;\"><ul><li>$compLabelStart$courseLabel$compLabelEnd$teachingLabel$timeAndRoom$coTeaching</li></ul></TD><TD style=\"border-left: 1px solid black;border-right: 1px solid black;\">".number_format(floatval($course['weighting']),3)."</TD></TR>";
                 $totalTeaching = $totalTeaching + floatval($course['weighting']);
