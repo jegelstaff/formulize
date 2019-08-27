556d031116267371c1faf85f66e9f2c5bb5f3768
Julian Egelstaff <nospam@yourturn.ca>
2019-08-27 10:43:23 -0400

    Fixing constructor again, ack!

--- class/xoopsform/formelementtray.php
+++ class/xoopsform/formelementtray.php
@@ -36,7 +36,7 @@ if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
 class XoopsFormElementTray extends icms_form_elements_Tray {
 	private $_deprecated;
 	public function __construct($caption, $delimeter = "&nbsp;", $name = "") {
-		$this->__construct($caption, $delimeter, $name);
+		parent::__construct($caption, $delimeter, $name);
 		$this->_deprecated = icms_core_Debug::setDeprecated('icms_form_elements_Tray', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
