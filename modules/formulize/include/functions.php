aba1f7bf518cb9cbe0e8dbf202982ec1237177bf
Julian Egelstaff <nospam@yourturn.ca>
2020-08-19 21:58:47 -0400

    Adding header info back into downloads! Ack!

--- modules/formulize/include/functions.php
+++ modules/formulize/include/functions.php
@@ -7328,6 +7328,8 @@ function export_data($queryData, $frid, $fid, $groups, $columns, $include_metada
 
     }
     
+    list($columns, $headers, $explodedColumns, $superHeaders) = export_prepColumns($columns,$include_metadata);
+    
     // output export header
     $destination = $output_filename ? XOOPS_ROOT_PATH.'/modules/formulize/export/'.$output_filename : 'php://output'; // open a file handle to stdout if we're not making an actual file, because fputcsv() needs something to attach to
     $output_handle = fopen($destination, 'w');
