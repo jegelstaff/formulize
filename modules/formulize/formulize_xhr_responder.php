34e8c36d6a6450c61b1328f6fec8ad03632652f4
Julian Egelstaff <nospam@yourturn.ca>
2020-10-13 12:34:13 -0400

    Recalculate text for display elements on conditions, not just derived

--- modules/formulize/formulize_xhr_responder.php
+++ modules/formulize/formulize_xhr_responder.php
@@ -251,8 +251,16 @@ switch($op) {
         $element_handler = xoops_getModuleHandler('elements', 'formulize');
         $formObject = $form_handler->get($formID);
         foreach($formObject->getVar('elementTypes') as $elementId=>$elementType) {
-            if($elementType == 'derived') {
-                $elementObject = $element_handler->get($elementId);
+            $elementObject = $element_handler->get($elementId);
+            $ele_value = $elementObject->getVar('ele_value');
+            // if it's derived, or it's text for display and the text for display has dynamic references, then render it and send it back
+            if($elementType == 'derived' OR (
+                (
+                    $elementType == 'areamodif' OR $elementType == 'ib') AND (
+                       strstr($ele_value[0], "\$value=") OR strstr($ele_value[0], "\$value =") OR (strstr($ele_value[0], "{") AND strstr($ele_value[0], "}"))
+                    )
+                )
+            ) {
                 if($html = renderElement($elementObject, $entryID)) {
                     $derivedValueMarkup[$elementId] = $html;
                 }
