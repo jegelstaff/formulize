be29440a8337d179f372cf42832c5f314dd0494d
Julian Egelstaff <nospam@yourturn.ca>
2020-10-26 10:26:35 -0400

    Fixing deprecated syntax, etc

--- libraries/htmlpurifier/HTMLPurifier.autoload.php
+++ libraries/htmlpurifier/HTMLPurifier.autoload.php
@@ -12,7 +12,7 @@ if (function_exists('spl_autoload_register') && function_exists('spl_autoload_un
         // Be polite and ensure that userland autoload gets retained
         spl_autoload_register('__autoload');
     }
-} elseif (!function_exists('__autoload')) {
+} elseif (!function_exists('spl_autoload_register') && !function_exists('spl_autoload_unregister') && !function_exists('__autoload')) {
     function __autoload($class) {
         return HTMLPurifier_Bootstrap::autoload($class);
     }
