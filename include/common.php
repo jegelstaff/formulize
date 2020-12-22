fa3aa2533b0d800c70337b75ad62496edf261088
Julian Egelstaff <nospam@yourturn.ca>
2020-12-22 18:46:19 -0500

    Removing deprecated functions
    Magic Quotes! ack!

--- include/common.php
+++ include/common.php
@@ -12,11 +12,6 @@
 /** make sure mainfile is included, for security and functionality */
 defined("XOOPS_MAINFILE_INCLUDED") or die();
 
-/** @todo This funtion has been deprecated in PHP 5.3.0 - need to adjust in 1.4 ~skenow */
-if (get_magic_quotes_runtime()) {
-    @set_magic_quotes_runtime(0);
-}
-
 // -- Include common functions and constants file
 require_once ICMS_ROOT_PATH . "/include/constants.php";
 include_once ICMS_INCLUDE_PATH . "/functions.php";
