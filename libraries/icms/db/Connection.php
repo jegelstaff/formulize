9b7a6d7660efc179ff40df6dc1b976d14bd660f8
Julian Egelstaff <jegelstaff@users.noreply.github.com>
2023-12-01 23:13:05 -0500

    Telling PHP the change in return type for the DB connection is OK (won't actually change, it's just PHP can't understand the dynamic call?)

--- libraries/icms/db/Connection.php
+++ libraries/icms/db/Connection.php
@@ -8,6 +8,7 @@ class icms_db_Connection extends PDO implements icms_db_IConnection {
 	public function escape($string) {
 		return substr($this->quote($string), 1, -1);
 	}
+	#[\ReturnTypeWillChange]
 	public function query(string $query, ?int $fetchMode = null, ...$fetchModeArgs) {
 		icms_Event::trigger('icms_db_IConnection', 'execute', $this, array('sql' => $query));
 		return call_user_func_array(array('parent', 'query'), func_get_args());
