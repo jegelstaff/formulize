1728b8bd179e9ab3edb4d7ac391984d7616b7baf
Arvin Singla <arvin.singla@gmail.com>
2024-02-21 10:21:13 -0500

    Only running one browser for now.

--- ci/playwright.config.js
+++ ci/playwright.config.js
@@ -33,10 +33,10 @@ module.exports = defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
 
     {
       name: 'firefox',
