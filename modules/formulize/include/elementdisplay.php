4d9298be25b2df70fdb2b9c072f5188e2dfa28b2
Julian Egelstaff <jegelstaff@users.noreply.github.com>
2024-02-26 19:46:34 -0500

    Adding IN support for display condition filters

--- modules/formulize/include/elementdisplay.php
+++ modules/formulize/include/elementdisplay.php
@@ -439,15 +439,18 @@ function buildEvaluationCondition($match,$indexes,$filterElements,$filterOps,$fi
 		} else {
 			$compValue = addslashes($compValue);
 		}
-        // in PHP 8 can't use empty strings for comparison in stristr because it will always give a false positive
+
+    // in PHP 8 can't use empty strings for comparison in stristr because it will always give a false positive
 		if($thisOp == "LIKE" AND addslashes($filterTerms[$i]) != '') {
 			$evaluationCondition .= "stristr('".$compValue."', '".addslashes($filterTerms[$i])."')";
 		} elseif($thisOp == "NOT LIKE" AND addslashes($filterTerms[$i]) != '') {
 			$evaluationCondition .= "!stristr('".$compValue."', '".addslashes($filterTerms[$i])."')";
-        } elseif($thisOp == "LIKE") {
-            $evaluationCondition .= $compValue ? 'FALSE' : 'TRUE';
-        } elseif($thisOp == "NOT LIKE") {
-            $evaluationCondition .= $compValue ? 'TRUE' : 'FALSE';
+    } elseif($thisOp == "LIKE") {
+      $evaluationCondition .= $compValue ? 'FALSE' : 'TRUE';
+    } elseif($thisOp == "NOT LIKE") {
+      $evaluationCondition .= $compValue ? 'TRUE' : 'FALSE';
+		} elseif($thisOp == "IN") {
+			$evaluationCondition .= "in_array('".$compValue."', array('".implode("','",array_filter(explode(',',str_replace("'", "\'", $filterTerms[$i])), 'trim'))."'))";
 		} else {
 			$evaluationCondition .= "'".$compValue."' $thisOp '".addslashes($filterTerms[$i])."'";
 		}
