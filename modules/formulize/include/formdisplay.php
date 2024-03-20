6074ef20dd887c65c075b746adbda94a66158fea
Julian Egelstaff <info@formulize.org>
2024-03-20 15:15:56 -0400

    Empty conditional HTML Capture dom element after capturing HTML. Duh!

--- modules/formulize/include/formdisplay.php
+++ modules/formulize/include/formdisplay.php
@@ -4318,7 +4318,9 @@ function callCheckCondition(name) {
 function captureDataAsInDOM(data) {
 	jQuery('#conditionalHTMLCapture').empty();
 	jQuery('#conditionalHTMLCapture').append(data);
-	return window.document.getElementById('conditionalHTMLCapture').innerHTML.trim();
+	var capturedHTML = window.document.getElementById('conditionalHTMLCapture').innerHTML.trim();
+	jQuery('#conditionalHTMLCapture').empty();
+	return capturedHTML;
 }
 
 function assignConditionalHTML(handle, data = '') {
