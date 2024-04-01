6723fa029e99d99dca195b63758de4951fe81f95
Julian Egelstaff <info@formulize.org>
2024-04-01 18:19:35 -0400

    Option 1 for adding autoadjustments to offet from floating tabs

--- modules/formulize/templates/screens/Anari/default/multiPage/bottomtemplate.php
+++ modules/formulize/templates/screens/Anari/default/multiPage/bottomtemplate.php
@@ -11,4 +11,40 @@ print "
 <div id='multipage-controls'>
 $previousPageButton $savePageButton $nextPageButton $pageIndicator $pageSelector
 </div>
+
+<script type='text/javascript'>
+
+function getCurrentHeightOfTabs() {
+	return jQuery('#pageNavTable').innerHeight();
+}
+
+function getDefaultHeightOfTabs() {
+	return jQuery('#pageNavTable > a.navtab').outerHeight(true);
+}
+
+function getTabFontSize() {
+	return parseInt(jQuery('#pageNavTable').css('font-size'));
+}
+
+function setFormOffsetFromTabs() {
+	let currentHeight = getCurrentHeightOfTabs();
+	let defaultHeight = getDefaultHeightOfTabs();
+	let offset = 0;
+	if(currentHeight > defaultHeight) {
+		let fontSize = getTabFontSize();
+		offset = Math.round((currentHeight - defaultHeight) / fontSize);
+	}
+	jQuery('#formulize_mainform > div.card').css('margin-top', offset+'em');
+}
+
+jQuery(document).ready(function() {
+	setFormOffsetFromTabs();
+	setTimeout(setFormOffsetFromTabs,500);
+});
+
+jQuery(window).resize(function () {
+	setFormOffsetFromTabs();
+});
+
+</script>
 ";
