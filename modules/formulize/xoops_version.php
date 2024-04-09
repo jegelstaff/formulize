6520b7b5cdd698de9bada649924af1c66a5cce27
Julian Egelstaff <info@formulize.org>
2024-04-09 00:38:36 -0400

    Version 7.3 RC1

--- modules/formulize/xoops_version.php
+++ modules/formulize/xoops_version.php
@@ -31,7 +31,7 @@
 
 $modversion = array(
 	'name' => _MI_formulize_NAME,
-	'version' => "7.21",
+	'version' => "7.3-RC1",
 	'description' => _MI_formulize_DESC,
 	'author' => "The Formulize Project - formulize.org",
 	'credits' => "",
