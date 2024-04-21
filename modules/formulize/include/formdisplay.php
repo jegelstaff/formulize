1e5bf66a7e3fffe148e48e1f8dcbf27ea56a24b5
Julian Egelstaff <info@formulize.org>
2024-04-21 00:15:06 -0400

    Moving autocomplete js to included file, and including it on list screens

--- modules/formulize/include/formdisplay.php
+++ modules/formulize/include/formdisplay.php
@@ -3932,42 +3932,9 @@ function removeTags(html) {
         print $output;
     }
 
-    // elementId is the hidden element we're interacting with - can be a series of elements with [] which would be the case if multiple is set
-    // value is the value we're setting
-    // change is a flag to indicate if we trigger a change on the element when we do this
-    // multiple indicates if this is a multi-select autocomplete
-    print "
-    function setAutocompleteValue(elementId, value, change, multiple) {
-        if(multiple) {
-          var targetElementId = 'last_selected_'+elementId;
-        } else {
-          var targetElementId = elementId;
-        }
-				jQuery('#'+targetElementId).val(value);
-        if(change && !multiple) {
-         	jQuery('#'+elementId).trigger('change');
-        }
-        formulizechanged=1;
-    }
-
-		function removeFromMultiValueAutocomplete(value, elementId) {
-			jQuery('#'+elementId+'_defaults input[value=\"'+value+'\"]').remove();
-			jQuery('.auto_multi_'+elementId+'[target=\"'+value+'\"]').remove();
-			triggerChangeOnMultiValueAutocomplete(elementId);
-		}
-
-		function triggerChangeOnMultiValueAutocomplete(elementId) {
-			var triggerElements = jQuery('[name=\"'+elementId+'[]\"]');
-			if(triggerElements.length == 0) {
-				jQuery('#'+elementId+'_defaults').append(\"<input type='hidden' name='\"+elementId+\"[]' jquerytag='\"+elementId+\"' id='\"+elementId+\"_0509' value='' />\");
-			}
-			jQuery('[name=\"'+elementId+'[]\"]').first().trigger('change');
-			jQuery('#'+elementId+'_0509').remove();
-			formulizechanged=1;
-		}
-
-    ";
-    print "</script>\n";
+    print "</script>
+		<script type='text/javascript' src='".XOOPS_URL."/modules/formulize/include/js/autocomplete.js'></script>
+		";
     $drawnJavascript = true;
 }
 
