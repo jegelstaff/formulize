6f875caa91ec1d44999933de0df5b0daa64587cb
Julian Egelstaff <jegelstaff@users.noreply.github.com>
2024-04-27 18:38:14 -0400

    Release 7.3 (#495)
    * changing version number in website template
    
    * Update index.html with new version number
    
    * Update xoops_version.php version number

--- modules/formulize/xoops_version.php
+++ modules/formulize/xoops_version.php
@@ -31,7 +31,7 @@
 
 $modversion = array(
 	'name' => _MI_formulize_NAME,
-	'version' => "7.3-RC2",
+	'version' => "7.3",
 	'description' => _MI_formulize_DESC,
 	'author' => "The Formulize Project - formulize.org",
 	'credits' => "",
