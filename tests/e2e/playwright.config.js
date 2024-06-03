3b714fb1b9692d85e28063f432a81104cd376a9a
Arvin Singla <arvin.singla@gmail.com>
2024-06-03 14:00:41 -0400

    Adjusting playwright config to more sane defaults for our needs.

--- tests/e2e/playwright.config.js
+++ tests/e2e/playwright.config.js
@@ -20,11 +20,13 @@ module.exports = defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: [
     ['html', {  outputFolder: './test-report' }]
   ],
+	/* Adjust the timeout for slow tests */
+	timeout: 120000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -34,12 +36,15 @@ module.exports = defineConfig({
     trace: 'on-first-retry',
 
 		/* Video */
-		video: 'on'
+		video: 'on',
+
+		/* Ignore HTTPS errors */
+		ignoreHTTPSErrors: true
   },
 
 	expect: {
     // Maximum time expect() should wait for the condition to be met.
-    timeout: 5000,
+    timeout: 30000,
 
     toHaveScreenshot: {
       // An acceptable amount of pixels that could be different, unset by default.
