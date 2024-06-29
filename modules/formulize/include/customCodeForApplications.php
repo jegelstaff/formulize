29f45561cadf14ec3f086a2bd53729f86cb2fba4
Julian Egelstaff <info@formulize.org>
2024-06-28 21:42:32 -0400

    Avoiding application handler when including custom code for applications

--- modules/formulize/include/customCodeForApplications.php
+++ modules/formulize/include/customCodeForApplications.php
@@ -4,12 +4,14 @@
  *
  *Added By Jinfu Jan 2015
  */
-$application_handler = xoops_getmodulehandler('applications','formulize');
-$apps = $application_handler->getAllApplications();
-
-foreach($apps as $appObject){
-   $aid=$appObject->getVar('appid');
-   if(file_exists(XOOPS_ROOT_PATH.'/modules/formulize/custom_code/application_custom_code_'.$aid.'.php')) {
-       include_once(XOOPS_ROOT_PATH.'/modules/formulize/custom_code/application_custom_code_'.$aid.'.php');
-   }
+global $xoopsDB;
+// must do DB query because this can be invoked before the session is fully instantiated. Using Application handler may reference the user object which won't exist yet when this is invoked early.
+$sql = 'SELECT appid FROM '.$xoopsDB->prefix('formulize_applications');
+if($res = $xoopsDB->query($sql)) {
+	while($row = $xoopsDB->fetchRow($res)) {
+		$aid=$row[0];
+		if(file_exists(XOOPS_ROOT_PATH.'/modules/formulize/custom_code/application_custom_code_'.$aid.'.php')) {
+				include_once(XOOPS_ROOT_PATH.'/modules/formulize/custom_code/application_custom_code_'.$aid.'.php');
+		}
+	}
 }
