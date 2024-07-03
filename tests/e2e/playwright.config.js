584f9dba6ee4488501a27ad4e9de242743b83953
Julian Egelstaff <info@formulize.org>
2024-07-03 12:21:30 -0400

    No retries ever (playwright default behaviour)

--- tests/e2e/playwright.config.js
+++ tests/e2e/playwright.config.js
@@ -17,8 +17,6 @@ module.exports = defineConfig({
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
