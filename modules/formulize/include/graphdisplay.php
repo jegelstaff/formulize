bdcedb152628740e7bf0b5de92292e3ebeabd2ff
Julian Egelstaff <info@formulize.org>
2024-07-25 17:11:21 -0400

    Fix out of range max value for Firefox (thanks amcharts!)

--- modules/formulize/include/graphdisplay.php
+++ modules/formulize/include/graphdisplay.php
@@ -256,7 +256,9 @@ function displayGraph($type, $data, $dataElements, $yElements, $xAxisType='time'
 
 				var rangeDataItem = yAxis.makeDataItem({
 					value: value,
-					endValue: value * 10000000
+				});
+				yAxis.onPrivate("max", (max) => {
+   				rangeDataItem.set("endValue", max * 2);
 				});
 
 				series.filledRanges.push(series.createAxisRange(rangeDataItem));
