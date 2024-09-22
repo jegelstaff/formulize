9a72f7e5039540f9dd2b098e9074a3bf88bb2798
Julian Egelstaff <info@formulize.org>
2024-09-22 12:02:30 -0400

    Updating version number (8 in beta)

--- modules/formulize/xoops_version.php
+++ modules/formulize/xoops_version.php
@@ -31,7 +31,7 @@
 
 $modversion = array(
 	'name' => _MI_formulize_NAME,
-	'version' => "8.0-alpha",
+	'version' => "8.0-beta",
 	'description' => _MI_formulize_DESC,
 	'author' => "The Formulize Project - formulize.org",
 	'credits' => "",
