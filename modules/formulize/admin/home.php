84ea10f054f66adc087516006cdda10ae65cf337
Julian Egelstaff <info@formulize.org>
2024-10-05 00:05:37 -0400

    Updating icons, links in admin home page

--- modules/formulize/admin/home.php
+++ modules/formulize/admin/home.php
@@ -102,11 +102,11 @@ function readApplicationData($aid, $apps) {
         $apps[$i]['header'] .= '<a href="'.XOOPS_URL.'/modules/formulize/admin/ui.php?page=application&aid='.$aid.'&tab=settings"><i class="icon-config"></i> Settings</a>';
     }
     // menu entries link does not work!!  can't pass names with spaces?
-    $apps[$i]['header'] .= '<a href="'.XOOPS_URL.'/modules/formulize/admin/ui.php?page=application&aid='.$aid.'&tab=forms"><i class="icon-form"></i> Forms</a>
+    $apps[$i]['header'] .= '<a href="'.XOOPS_URL.'/modules/formulize/admin/ui.php?page=application&aid='.$aid.'&tab=forms"><i class="icon-form-mini"></i> Forms</a>
         <a href="'.XOOPS_URL.'/modules/formulize/admin/ui.php?page=application&aid='.$aid.'&tab=screens"><i class="icon-screen"></i> Screens</a>
         <a href="'.XOOPS_URL.'/modules/formulize/admin/ui.php?page=application&aid='.$aid.'&tab=relationships"><i class="icon-connection"></i> Relationships</a>
         <a href="'.XOOPS_URL.'/modules/formulize/admin/ui.php?page=application&aid='.$aid.'&tab=menu%20entries"><i class="icon-menu"></i> Menu Entries</a>
-        <a href="'.XOOPS_URL.'/modules/formulize/admin/ui.php?page=export&aid='.$aid.'"><i class="icon-download"></i> Export (beta!)</a>
+				<a href="'.XOOPS_URL.'/modules/formulize/admin/ui.php?page=application&aid='.$aid.'&tab=code"><i class="icon-screen-form"></i> Code</a>
         <a href="'.XOOPS_URL.'/modules/formulize/admin/ui.php?page=form&aid='.$aid.'&tab=settings&fid=new"><i class="icon-add"></i> Add a Form</a>';
     if ($aid>0) {
         $apps[$i]['header'] .= '<a href="" class="deleteapplink" target="'.$aid.'"><i class="icon-delete"></i> Delete</a>';
