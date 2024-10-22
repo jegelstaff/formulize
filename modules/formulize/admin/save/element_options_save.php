97bb44a208a52fec32f324cfc10e4dbd83ab0b1f
Julian Egelstaff <info@formulize.org>
2024-10-21 22:14:42 -0400

    Handling linked selectbox setup and also all element deletion

--- modules/formulize/admin/save/element_options_save.php
+++ modules/formulize/admin/save/element_options_save.php
@@ -185,16 +185,28 @@ if($ele_type == "select") {
     $res_link = $xoopsDB->query($sql_link);
     $array_link = $xoopsDB->fetchArray($res_link);
     $processedValues['elements']['ele_value'][2] = $array_link['id_form'] . "#*=:*" . $array_link['ele_handle'];
-  } else {
+		// ensure there is a primary relationship link representing this connection / update existing connection
+    // Get existing linked settings, if any
+		$currentLinkedFormId = 0;
+		$currentLinkedElementId = 0;
+		$currentEleValue = $element->getVar('ele_value');
+		if($element->isLinked) {
+				$currentEleValue2Parts = explode('#*=:*', $currentEleValue);
+				$currentLinkedFormId = $currentEleValue2Parts[0];
+				$currentLinkedElementId = $currentEleValue2Parts[1];
+		}
+		updateLinkedElementConnectionsInRelationships($element->getVar('fid'), $element->getVar('ele_id'), $array_link['id_form'], $_POST['formlink'], $currentLinkedFormId, $currentLinkedElementId);
+	} else {
     // a user requests to unlink the select box and select box is currently linked
     if ($_POST['formlink'] == "none" AND $element->isLinked){
       $form_handler->updateField($element, $element->getVar("ele_handle"), "text");
+			// remove any primary relationship link representing this connection
+			deleteElementConnectionsInRelationships($element->getVar('fid'), $element->getVar('ele_id'));
     }
-
     list($_POST['ele_value'], $processedValues['elements']['ele_uitext']) = formulize_extractUIText($_POST['ele_value']);
     foreach($_POST['ele_value'] as $id=>$text) {
       if($text !== "") {
-	$processedValues['elements']['ele_value'][2][$text] = isset($_POST['defaultoption'][$id]) ? 1 : 0;
+				$processedValues['elements']['ele_value'][2][$text] = isset($_POST['defaultoption'][$id]) ? 1 : 0;
       }
     }
   }
