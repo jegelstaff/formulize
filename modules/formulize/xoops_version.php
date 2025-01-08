beefb0dda998959ffaebd0f9cca8293bfc562960
Julian Egelstaff <info@formulize.org>
2025-01-07 23:12:54 -0500

    Adding Create Connection dialog, and triggering on draggable snap!

--- modules/formulize/xoops_version.php
+++ modules/formulize/xoops_version.php
@@ -727,6 +727,9 @@ $modversion['templates'][] = array(
 $modversion['templates'][] = array(
 	'file' => 'admin/relationship_options.html',
 	'description' => '');
+$modversion['templates'][] = array(
+	'file' => 'admin/relationship_create_connection.html',
+	'description' => '');
 
 //	Module Configs
 // $xoopsModuleConfig['t_width']
