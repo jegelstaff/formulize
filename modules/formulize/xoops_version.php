06761a9dc39d63ba439fabc220fe530b60ddcab0
Julian Egelstaff <info@formulize.org>
2025-02-15 16:41:33 -0500

    Updating version number to 7.43

--- modules/formulize/xoops_version.php
+++ modules/formulize/xoops_version.php
@@ -31,7 +31,7 @@
 
 $modversion = array(
 	'name' => _MI_formulize_NAME,
-	'version' => "8.0-beta",
+	'version' => "7.43",
 	'description' => _MI_formulize_DESC,
 	'author' => "The Formulize Project - formulize.org",
 	'credits' => "",
