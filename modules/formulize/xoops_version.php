ac078e6db223305e05f5181c67d2f4198446dc91
Julian Egelstaff <info@formulize.org>
2025-02-15 16:43:24 -0500

    Setting version number back to 8 beta for development

--- modules/formulize/xoops_version.php
+++ modules/formulize/xoops_version.php
@@ -31,7 +31,7 @@
 
 $modversion = array(
 	'name' => _MI_formulize_NAME,
-	'version' => "7.43",
+	'version' => "8.0-beta",
 	'description' => _MI_formulize_DESC,
 	'author' => "The Formulize Project - formulize.org",
 	'credits' => "",
