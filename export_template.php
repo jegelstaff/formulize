cffcebbd05a8252202f561d1c0690cbebf8982aa
Julian Egelstaff <info@formulize.org>
2025-02-18 23:29:25 -0500

    Removing unnecessary file

--- export_template.php
+++
@@ -1,8 +0,0 @@
-<?php
-// code modified from export.php where the template is made
-include_once "mainfile.php";
-$formulize_doingManualExport = true;
-include XOOPS_ROOT_PATH."/modules/formulize/include/export.php";
-$queryData = file(XOOPS_ROOT_PATH."/cache/exportQuery_".intval($_GET['eq']).".formulize_cached_query_for_export");
-$groups = $xoopsUser ? $xoopsUser->getGroups() : array(0=>XOOPS_GROUP_ANONYMOUS);
-do_update_export($queryData, intval($_GET['frid']), intval($_GET['fid']), $groups); // needs $_GET['cols'] 
