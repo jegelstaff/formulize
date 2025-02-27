jegelstaff/formulize

commit 0ce8e883e82223d3c97e3d279a34ecf708b345f0

Various Grid element improvements (#438)
* Don't display empty grid cells when not necessary

* Adding spacing for grid headers

* Getting grids to respect conditions -- needs refactoring!

* Marking grid elements as required if constituent elements are required

* Cleaning up errant merge

* Final refactoring for grids! Epic!!

* Cleaning up merge issue

* Further merge tidying

* Remove display conditions from elements in grids, if the grid has its display conditions removed

* Final integration of disabled and display conditions, in and out of grid display of elements

libraries/icms/form/Element.php
@@ -298,7 +298,7 @@ abstract class icms_form_Element {
 		if (!empty($this->customValidationCode)) {
 			return implode("\n", $this->customValidationCode);
 			// generate validation code if required
-		} elseif ($this->isRequired()) {
+		} elseif ($this->isRequired() AND !is_a($this, 'icms_form_elements_Label')) {
 			$eltname    = $this->getName();
 			$eltcaption = $this->getCaption();
 			$eltmsg = empty($eltcaption)

modules/formulize/admin/save/element_display_save.php
@@ -226,6 +226,21 @@ if(!$ele_id = $element_handler->insert($element)) {
   print "Error: could not save the display settings for element: ".$xoopsDB->error();
 }
 
+// if this is a grid element, set the display conditions for the contained elements
+if($element->getVar('ele_type')=='grid') {
+	$ele_value = $element->getVar('ele_value');
+	$gridCount = count(explode(",", $ele_value[1])) * count(explode(",", $ele_value[2]));
+	include_once XOOPS_ROOT_PATH.'/modules/formulize/include/griddisplay.php';
+	foreach(elementsInGrid($ele_value[4], $element->getVar('id_form'), $gridCount) as $gridElementId) {
+		$gridElementObject = $element_handler->get($gridElementId);
+		$gridElementObject->setVar('ele_filtersettings', $parsedFilterSettings);
+		if(!$element_handler->insert($gridElementObject)) {
+			$elementLabel = $gridElementObject->getVar('ele_colhead') ? $gridElementObject->getVar('ele_colhead') : $gridElementObject->getVar('ele_caption');
+			print "Error: could not apply grid display settings to the element ".$elementLabel."\n";
+		}
+	}
+}
+
 if($_POST['reload_element_pages']) {
   print "/* evalnow */ if(redirect=='') { redirect = 'reloadWithScrollPosition();'; }";
 }

modules/formulize/formulize_xhr_responder.php
@@ -315,38 +315,47 @@ switch($op) {
 
 function renderElement($elementObject, $entryId) {
 
-    include_once XOOPS_ROOT_PATH . "/modules/formulize/include/elementdisplay.php";
-    // "" is framework, ie: not applicable
-    $GLOBALS['formulize_asynchronousRendering'][$elementObject->getVar('ele_handle')] = true;
-    $deReturnValue = displayElement("", $elementObject, $entryId, false, null, null, false); // false, null, null, false means it's not a noSave element, no screen, no prevEntry data passed in, and do not render the element on screen
-    unset($GLOBALS['formulize_asynchronousRendering']);
-    if(is_array($deReturnValue)) {
-        if($deReturnValue[0] == 'hidden') {
-            if(is_object($deReturnValue[2])) {
-                return $deReturnValue[2]->render();
-            }
-        } else {
-            $form_ele = $deReturnValue[0];
-            if($elementObject->getVar('ele_req') AND is_object($form_ele)) {
-                $form_ele->setRequired();
-            }
-            $isDisabled = $deReturnValue[1];
-            require_once XOOPS_ROOT_PATH."/modules/formulize/include/formdisplay.php"; // need the formulize_themeForm
-            $form = new formulize_themeForm('formulizeAsynchElementRender','',''); // prepare empty form object just for rendering element
-            if($elementObject->getVar('ele_type') == "ib") {// if it's a break, handle it differently...
-                $entryForDEElements = (is_numeric($entryId) AND $entryId) ? $entryId : 'new';
-                $form->insertBreakFormulize("<div class=\"formulize-text-for-display\">" . trans(stripslashes($form_ele[0])) . "</div>", $form_ele[1], 'de_'.$elementObject->getVar('id_form').'_'.$entryForDEElements.'_'.$elementObject->getVar('ele_id'), $elementObject->getVar("ele_handle"));
-                $hidden = '';
-                $html = '';
-                list($html, $hidden) = $form->_drawElements($form->getElements(), $html, $hidden);
-            } else {
-              $html = $form->_drawElementElementHTML($form_ele);
-            }
-            if($html) {
-                $html = trans($html);
-                return $html;
-            }
-        }
-    }
-    return false;
+	$GLOBALS['formulize_asynchronousRendering'][$elementObject->getVar('ele_handle')] = true;
+	$deReturnValue = displayElement("", $elementObject, $entryId, false, null, null, false); // false, null, null, false means it's not a noSave element, no screen, no prevEntry data passed in, and do not render the element on screen
+	unset($GLOBALS['formulize_asynchronousRendering']);
+
+	// element is allowed, so prep some stuff for rendering...
+	if(is_array($deReturnValue)) {
+		$form_ele = $deReturnValue[0];
+		if($elementObject->getVar('ele_req') AND is_object($form_ele)) {
+				$form_ele->setRequired();
+		}
+		$isDisabled = $deReturnValue[1];
+		$elementContents = $form_ele;
+
+		// prepare empty form object just for rendering element
+		$form = new formulize_themeForm('formulizeAsynchElementRender','','');
+
+		// figure out what we've got on our hands to render
+		$breakClass = 'head';
+		$entryForDEElements = (is_numeric($entryId) AND $entryId) ? $entryId : 'new';
+		if($elementObject->getVar('ele_type') == "ib") {
+			$elementContents = "<div class=\"formulize-text-for-display\">" . trans(stripslashes($form_ele[0])) . "</div>";
+			$breakClass = $form_ele[1];
+		} elseif($elementObject->getVar('ele_type') == "grid") {
+			$elementContents = renderGrid($elementObject, $entryForDEElements); // won't take into account the existing entry's saved values or the screen config when rendering the consituent elements, but probably doesn't matter.
+		}
+
+		// render the element
+		if(is_object($elementContents)) {
+			$html = $form->_drawElementElementHTML($elementContents);
+		} else {
+			$form->insertBreakFormulize($elementContents, $breakClass, 'de_'.$elementObject->getVar('id_form').'_'.$entryForDEElements.'_'.$elementObject->getVar('ele_id'), $elementObject->getVar("ele_handle"));
+			$hidden = '';
+			$html = '';
+			list($html, $hidden) = $form->_drawElements($form->getElements(), $html, $hidden);
+		}
+
+		// return the html, or nothing
+		if($html) {
+			$html = trans($html);
+			return $html;
+		}
+	}
+	return false;
 }

modules/formulize/include/common.php
@@ -50,6 +50,7 @@ include_once formulize_ROOT_PATH.'include/entriesdisplay.php';
 include_once formulize_ROOT_PATH.'include/graphdisplay.php';
 include_once formulize_ROOT_PATH.'include/calendardisplay.php';
 include_once formulize_ROOT_PATH.'include/elementdisplay.php';
+include_once formulize_ROOT_PATH.'include/griddisplay.php';
 include_once formulize_ROOT_PATH.'include/extract.php';
 include_once formulize_ROOT_PATH.'include/customCodeForApplications.php';
 

modules/formulize/include/functions.php
@@ -316,6 +316,11 @@ function availReports($uid, $groups, $fid, $frid="0") {
 // security check to see if a form is allowed for the user:
 function security_check($form_id, $entry_id="", $user_id="", $owner="", $groups="", $mid="", $gperm_handler="") {
 
+		static $cachedSecurityChecks = array();
+		if(isset($cachedSecurityChecks[$form_id][$entry_id])) {
+			return $cachedSecurityChecks[$form_id][$entry_id];
+		}
+
     if (!$mid) { // if no mid specified, set it
         $mid = getFormulizeModId();
     }
@@ -344,27 +349,26 @@ function security_check($form_id, $entry_id="", $user_id="", $owner="", $groups=
     }
 
     if (!$gperm_handler->checkRight("view_form", $form_id, $groups, $mid)) {
+				$cachedSecurityChecks[$form_id][$entry_id] = false;
         return false;
     }
 
     if ($entry_id == "proxy" AND !$gperm_handler->checkRight("add_proxy_entries", $form_id, $groups, $mid)) {
+				$cachedSecurityChecks[$form_id][$entry_id] = false;
         return false;
     }
 
     if ($entry_id == "new" AND !$gperm_handler->checkRight("add_own_entry", $form_id, $groups, $mid)) {
+				$cachedSecurityChecks[$form_id][$entry_id] = false;
         return false;
-    }
-
-    if($entry_id == "new" OR $entry_id == "proxy") {
-        $entry_id = ""; // if this is a new entry, then we don't do the check below to look for permissions on a specific entry, since there isn't one yet!
     }
 
     // do security check on entry in form -- note: based on the initial entry passed, does not consider entries in one-to-one linked forms which are assumed to be allowed for the user if the main entry is.
     // allow user to see own entry
     // any entry if they have view_globalscope
     // other users in the appropriate group if they have view_groupscope
-    // --report overrides need to be added in here for display of entries in reports
-    if ($entry_id) {
+    // --report overrides added in here for display of entries in reports
+    if ($entry_id AND $entry_id != "new" AND $entry_id != "proxy") {
         $view_globalscope = $gperm_handler->checkRight("view_globalscope", $form_id, $groups, $mid);
         if (!$view_globalscope) {
 
@@ -393,8 +397,10 @@ function security_check($form_id, $entry_id="", $user_id="", $owner="", $groups=
                         $candidateEntries[] = intval($_COOKIE['entryid_'.$form_id]);
                     }
                     if(in_array($entry_id, $candidateEntries)) {
+												$cachedSecurityChecks[$form_id][$entry_id] = true;
                         return true;
                     }
+										$cachedSecurityChecks[$form_id][$entry_id] = false;
                     return false;
                 }
             } elseif ($owner != $user_id) {
@@ -427,15 +433,18 @@ function security_check($form_id, $entry_id="", $user_id="", $owner="", $groups=
                         if (array_intersect($pubbedgroups, $groups)) {
                             // user has been published an unlocked view for which the scope is all
                             if ($thisview['sv_currentview'] == "all") {
+																$cachedSecurityChecks[$form_id][$entry_id] = true;
                                 return true;
                             }
                             // what about groupscope in the view?  is that accounted for below, or should we check against "group"??
                             $viewgroups = explode(",", $thisview['sv_currentview']);
                             if (array_intersect($data_handler->getEntryOwnerGroups($entry_id), $viewgroups)) {
+																$cachedSecurityChecks[$form_id][$entry_id] = true;
                                 return true;
                             }
                         }
                     }
+										$cachedSecurityChecks[$form_id][$entry_id] = false;
                     return false;
                 }
             }
@@ -448,15 +457,18 @@ function security_check($form_id, $entry_id="", $user_id="", $owner="", $groups=
             global $xoopsDB;
             $checkSQL = "SELECT count(entry_id) FROM ".$xoopsDB->prefix("formulize_".$formObject->getVar('form_handle'))." WHERE entry_id = $entry_id $perGroupFilter";
             if (!$checkRes = $xoopsDB->query($checkSQL)) {
+								$cachedSecurityChecks[$form_id][$entry_id] = false;
                 return false;
             }
             $countRow = $xoopsDB->fetchRow($checkRes);
             if ($countRow[0] != 1) {
+								$cachedSecurityChecks[$form_id][$entry_id] = false;
                 return false;
             }
         }
     }
 
+		$cachedSecurityChecks[$form_id][$entry_id] = true;
     return true;
 }
 
@@ -2199,7 +2211,16 @@ function getElementValue($entry, $element_id, $fid) {
 
 
 // this function checks for singleentry status and returns the appropriate entry in the form if there is one
-function getSingle($fid, $uid, $groups, $member_handler, $gperm_handler, $mid) {
+function getSingle($fid, $uid, $groups, $member_handler=null, $gperm_handler=null, $mid=null) {
+		if(!$member_handler) {
+			$member_handler = xoops_gethandler('member');
+		}
+		if(!$gperm_handler) {
+			$gperm_handler = xoops_gethandler('groupperm');
+		}
+		if(!$mid) {
+			$mid = getFormulizeModId();
+		}
     global $xoopsDB, $xoopsUser;
     // determine single/multi status
     $smq = q("SELECT singleentry FROM " . $xoopsDB->prefix("formulize_id") . " WHERE id_form=$fid");

commit 0c0db68486d8c19dffc378b155f919d7e1b37a20

Better req js for elements with no custom js
fixes issue #623, introduced in 0ce8e883e82223d3c97e3d279a34ecf708b345f0 by lazy checking if an element is a label, instead of properly looking for grids. All elements are rendered in labels!

libraries/icms/form/Element.php
@@ -298,7 +298,7 @@ abstract class icms_form_Element {
 		if (!empty($this->customValidationCode)) {
 			return implode("\n", $this->customValidationCode);
 			// generate validation code if required
-		} elseif ($this->isRequired() AND !is_a($this, 'icms_form_elements_Label')) {
+		} elseif ($this->isRequired() AND (!property_exists($this, 'formulize_element') OR $this->formulize_element->getVar('ele_type') != 'grid')) {
 			$eltname    = $this->getName();
 			$eltcaption = $this->getCaption();
 			$eltmsg = empty($eltcaption)