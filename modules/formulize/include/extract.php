2b4b386e08e8a446a9a4c678b91f6f974a5c82a5
Julian Egelstaff <info@formulize.org>
2025-03-21 15:31:14 -0400

    Update derived value file/function names to work with primary relationship

--- modules/formulize/include/extract.php
+++ modules/formulize/include/extract.php
@@ -2162,7 +2162,8 @@ function formulize_calcDerivedColumns($entry, $metadata, $relationship_id, $form
 					} // datasets can contain empty values for subforms, etc, when no entries exist. We must not process phantom non-existent entries.
 					$dataToWrite = array();
 					foreach ($metadata[$formHandle] as $formulaNumber => $thisMetaData) {
-						$functionName = "derivedValueFormula_" . str_replace(array(" ", "-", "/", "'", "`", "\\", ".", "�", ",", ")", "(", "[", "]"), "_", trans($formHandle, 'en')) . "_" . $relationship_id . "_" . $form_id . "_" . $formulaNumber;
+						$fridForName = $relationship_id == -1 ? 'primary' : $relationship_id;
+						$functionName = "derivedValueFormula_" . str_replace(array(" ", "-", "/", "'", "`", "\\", ".", "�", ",", ")", "(", "[", "]"), "_", trans($formHandle, 'en')) . "_" . $fridForName. "_" . $form_id . "_" . $formulaNumber;
 						// want to turn off the derived value update flag for the actual processing of a value, since the function might have a getData call in it!!
 						$resetDerivedValueFlag = false;
 						if (isset($GLOBALS['formulize_forceDerivedValueUpdate'])) {
@@ -2236,10 +2237,11 @@ function formulize_includeDerivedValueFormulas($metadata, $formHandle, $frid, $f
 			}
 			$formula = implode("\n", $formulaLines);
 		}
-		$fileName = XOOPS_ROOT_PATH . '/modules/formulize/cache/Derived_value_formula_for_' . trans($thisMetaData['handle'], 'en') . '_in_form_' . $thisMetaData['form_id'] . "_(fid_" . $fid . "_frid_" . $frid . "_fn_" . $formulaNumber . ").php";
+		$fridForName = $frid == -1 ? 'primary' : $frid;
+		$fileName = XOOPS_ROOT_PATH . '/modules/formulize/cache/Derived_value_formula_for_' . trans($thisMetaData['handle'], 'en') . '_in_form_' . $thisMetaData['form_id'] . "_(fid_" . $fid . "_frid_" . $fridForName . "_fn_" . $formulaNumber . ").php";
 		$formula = removeOpeningPHPTag($formula);
 		file_put_contents($fileName, "<?php
-    function derivedValueFormula_" . str_replace(array(" ", "-", "/", "'", "`", "\\", ".", "�", ",", ")", "(", "[", "]"), "_", trans($formHandle, 'en')) . "_" . $frid . "_" . $fid . "_" . $formulaNumber . "(\$entry, \$form_id, \$entry_id, \$relationship_id) {
+    function derivedValueFormula_" . str_replace(array(" ", "-", "/", "'", "`", "\\", ".", "�", ",", ")", "(", "[", "]"), "_", trans($formHandle, 'en')) . "_" . $fridForName . "_" . $fid . "_" . $formulaNumber . "(\$entry, \$form_id, \$entry_id, \$relationship_id) {
         $formula
         return \$value;
     }\r\n");
