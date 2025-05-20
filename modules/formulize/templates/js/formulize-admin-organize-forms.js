2fe95c55f505b4cfd7aeaeaf3f840827d530de57
Julian Egelstaff <info@formulize.org>
2025-05-20 14:32:10 -0400

    Accordion sections instead of popup, for list-by-name view of forms

--- modules/formulize/templates/js/formulize-admin-organize-forms.js
+++ modules/formulize/templates/js/formulize-admin-organize-forms.js
@@ -203,14 +203,31 @@ function gatherAllConnectedBoxes(appid, form1Id, form2Ids) {
 function clickFormDetails(jQFormDetailsBox) {
 	var formid = jQFormDetailsBox.attr('formid');
 	var appid = jQFormDetailsBox.attr('appid');
+	// clicked on the same form again, so close
 	if($('#form-details-box-'+appid+'-'+formid).hasClass('selected-form')) {
-		$('#form-details-box-'+appid+'-'+formid).toggleClass('selected-form');
-		$('#form-listing-details-'+appid+'-'+formid).hide(250);
+		$('#form-listing-details-'+appid+'-'+formid).hide(250, "linear");
+		setTimeout(function() {
+			$('#form-details-box-'+appid+'-'+formid).removeClass('selected-form');
+			$('#form-details-box-'+appid+'-'+formid).removeClass('open-accordion-listing');
+			$('#form-listing-details-'+appid+'-'+formid).removeClass('open-accordion-listing');
+			$('#form-listing-details-'+appid+'-'+formid).removeClass('current-organize-custom');
+		}, 250);
+
+	// clicked on a form first time, so open
 	} else {
 		$('div[id^=form-details-box]').removeClass('selected-form');
-		$('#form-details-box-'+appid+'-'+formid).toggleClass('selected-form');
-		$('#form-listing-details-'+appid+'-'+formid).show(250);
+		$('div[id^=form-details-box-]').removeClass('open-accordion-listing');
+		$('div[id^=form-listing-details-]').removeClass('open-accordion-listing');
+		$('div[id^=form-listing-details-]').removeClass('current-organize-custom');
+		$('#form-details-box-'+appid+'-'+formid).addClass('selected-form');
+		if(currentOrganize != 'custom') {
+			$('#form-details-box-'+appid+'-'+formid).addClass('open-accordion-listing');
+			$('#form-listing-details-'+appid+'-'+formid).addClass('open-accordion-listing');
+		} else {
+			$('#form-listing-details-'+appid+'-'+formid).addClass('current-organize-custom');
+		}
 		$('div[id^=form-listing-details-]:not(#form-listing-details-'+appid+'-'+formid+')').hide();
+		$('#form-listing-details-'+appid+'-'+formid).show(250, "linear");
 	}
 	return false;
 }
@@ -240,31 +257,38 @@ function filterClickAndDrag(appid, filterText) {
 function applyCSSAlpha() {
 	$('div.form-name').removeClass('ui-corner-top');
 	$('div.form-name').addClass('ui-corner-left');
+	$('div.form-screen-list-outer').css('display', 'flex');
 	$('div.form-screen-list').removeClass('ui-corner-bottom');
 	$('div.form-screen-list').addClass('ui-corner-right');
 	$('div.form-screen-list').css('border-left', '0');
 	$('div.form-screen-list').css('border-top', '1px solid #94daf2');
 	$('div[id^=form-details-box]').css('display', 'flex');
-	$('div[id^=form-details-box]').css('flex-direction', 'row');
+	$('div[id^=form-details-box]').css('flex-direction', 'column');
 	$('div[id^=form-details-box]').css('width', 'auto');
 	$('div[id^=form-details-box]').css('margin-bottom', '0');
 	$('div.form-name').css('width', '340px');
 	$('div.form-screen-list').css('width', '100%');
 	$('div.form-data').css('display', 'flex');
+	$('.organize-toggle').css('z-index', '2');
 
 }
 
 function applyCSSCustom() {
 	$('div.form-name').addClass('ui-corner-top');
 	$('div.form-name').removeClass('ui-corner-left');
+	$('div.form-screen-list-outer').css('display', 'block');
 	$('div.form-screen-list').addClass('ui-corner-bottom');
 	$('div.form-screen-list').removeClass('ui-corner-right');
 	$('div.form-screen-list').css('border-top', '0');
 	$('div.form-screen-list').css('border-left', '1px solid #94daf2');
+	$('div[id^=form-details-box]').css('display', 'flex');
+	$('div[id^=form-details-box]').css('flex-direction', 'row');
 	$('div[id^=form-details-box]').css('display', 'block');
 	$('div[id^=form-details-box]').css('width', '340px');
 	$('div[id^=form-details-box]').css('margin-bottom', '1.5em');
 	$('div.form-name').css('width', 'auto');
 	$('div.form-screen-list').css('width', 'auto');
 	$('div.form-data').css('display', 'none');
+	$('.organize-toggle').css('z-index', '1');
+
 }
