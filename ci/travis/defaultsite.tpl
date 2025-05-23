3a995707513bdadd119a0b6d5baf2ee0dfbc8a55
Julian Egelstaff <nospam@yourturn.ca>
2025-05-23 08:53:20 -0300

    Removing old theme, and old CI stuff

--- ci/travis/defaultsite.tpl
+++
@@ -1,9 +0,0 @@
-<VirtualHost *:80>
-	ServerName local.dev
-	DocumentRoot /var/www
-	ErrorLog /tmp/error.log
-<Directory "/var/www">
-Options +Includes
-Options +FollowSymLinks -Indexes
-</Directory>
-</VirtualHost>
--- ci/travis/selenium_debug.js
+++
@@ -1,4 +0,0 @@
-// this file is copied to modules/formulize/selenium_debug.js
-// SELENIUM_DEBUG turns off some javascript features (currently just CodeMirror)
-// that interfere with selenium tests.
-SELENIUM_DEBUG = 'ON';
