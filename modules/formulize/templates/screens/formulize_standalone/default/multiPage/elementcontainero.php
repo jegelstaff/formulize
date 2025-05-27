e78f490d1a680f73152b9baecc58bd13b18b8846
Julian Egelstaff <info@formulize.org>
2025-05-27 16:02:25 -0400

    Remove formulize_standalone from templates folder in Formulize module

--- modules/formulize/templates/screens/formulize_standalone/default/multiPage/elementcontainero.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$style = "";
-if($startHidden) {
-    $style = "style='display: none;'";
-}
-
-print "<tr id='".$elementContainerId."' $style class='".$elementClass."' valign='top' align='" . _GLOBAL_LEFT . "'>
-";
