1743a196f0b891985d2ad1c7f41da1b0890a6154
Julian Egelstaff <julian@formulize.net>
2025-06-16 17:47:30 -0400

    Add min PHP version of Formulize modinfo and check it

--- modules/formulize/xoops_version.php
+++ modules/formulize/xoops_version.php
@@ -32,6 +32,8 @@
 $modversion = array(
 	'name' => _MI_formulize_NAME,
 	'version' => "8.0-RC1",
+	'min_php_version' => "8.1",
+	'min_php_version_id' => "80100",
 	'description' => _MI_formulize_DESC,
 	'author' => "The Formulize Project - formulize.org",
 	'credits' => "",
