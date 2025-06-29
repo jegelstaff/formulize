d6ed1100b8037426fc02760ff0bb8cfe3a984a70
Julian Egelstaff <julian@formulize.net>
2025-06-29 05:23:38 -0400

    First pass at adding preference

--- modules/formulize/language/english/modinfo.php
+++ modules/formulize/language/english/modinfo.php
@@ -154,6 +154,20 @@ foreach($formulizeConfig as $thisConfig=>$thisConfigValue) {
 define("_MI_formulize_PUBLICAPIENABLED", "Enable the Public API".$publicAPIInstructions);
 define("_MI_formulize_PUBLICAPIENABLED_DESC", "When this is enabled, you can use the Public API documented at https://formulize.org/developers/public-api/");
 
+$mcpServerInstructions = '';
+foreach($formulizeConfig as $thisConfig=>$thisConfigValue) {
+	if($thisConfig == 'formulizeMCPServerEnabled' AND $thisConfigValue == 0) {
+		$mcpServerInstructions = "<br><br>For the MCP Server to work, you will need to add this code to the .htaccess file at the root of your website. Make sure to put it after any other rewrite rules.
+		<blockquote style=\"font-weight: normal; font-family: monospace; white-space: nowrap;\">
+		# Necessary for HTTP Authorization header to be passed through to the MCP server<br>
+		RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]<br>
+		</blockquote><i>If you enabled this option, but these instructions are still here, and the option is off again, then your server is not yet properly configured for the MCP Server.</i>";
+		break;
+	}
+}
+define("_MI_formulize_MCPSERVERENABLED", "Enable the MCP Server for working with AI<br><br><a href='https://formulize.org/ai' target='_blank'>Read more about MCP setup on https://formulize.org</a>.".$mcpServerInstructions);
+define("_MI_formulize_MCPSERVERENABLED_DESC", "MCP (Model Context Protocol) is a way of connecting AI assistants, like Claude, Gemini, Copilot, etc, to Formulize. With MCP, AI assistants can read information from Formulize and help you configure Formulize.");
+
 define("_MI_formulize_REVISIONSFORALLFORMS", "Turn on revision history for all forms");
 define("_MI_formulize_REVISIONSFORALLFORMS_DESC", "Normally, you can turn on revision history for each form as you see fit. If you want to turn it on for all forms always, turn this preference on, and the option will be disabled in each form's settings.");
 
