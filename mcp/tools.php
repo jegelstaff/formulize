d3c5c855f49dd59e763a162c6bf0288a89d651ba
Julian Egelstaff <julian@formulize.net>
2025-08-29 11:58:18 -0400

    Improving/fixing error responses

--- mcp/tools.php
+++ mcp/tools.php
@@ -695,7 +695,7 @@ Examples:
 				]
 			];
 		} catch (Exception $e) {
-			throw $e;
+			new FormulizeMCPException($e->getMessage(), 'database_error');
 		}
 	}
 
@@ -1037,7 +1037,7 @@ private function validateFilter($filter) {
 
 			return $response;
 		} catch (Exception $e) {
-			throw $e;
+			new FormulizeMCPException($e->getMessage(), 'database_error');
 		}
 	}
 
