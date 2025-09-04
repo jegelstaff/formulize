6fb9e7c41fd093dba7505148ae1c0e2834035d58
Julian Egelstaff <julian@formulize.net>
2025-09-04 10:49:34 -0400

    Fix sig on loadvalue call

--- modules/formulize/include/formdisplay.php
+++ modules/formulize/include/formdisplay.php
@@ -2906,7 +2906,7 @@ function loadValue($element, $entry_id, $prevEntry) {
 	$type = $element->getVar('ele_type');
 	if(file_exists(XOOPS_ROOT_PATH."/modules/formulize/class/".$type."Element.php")) {
 		$customTypeHandler = xoops_getmodulehandler($type."Element", 'formulize');
-		$ele_value = $customTypeHandler->loadValue($element, $value);
+		$ele_value = $customTypeHandler->loadValue($element, $value, $entry_id);
 	}
 
 	return $ele_value;
