1e88d62530192f7026b37701505e56ef34559489
Julian Egelstaff <julian@formulize.net>
2025-09-05 14:08:36 -0400

    Adding stubs of a couple new tests

---
+++ tests/e2e/formulize-core/020-create-elements.spec.js
@@ -0,0 +1,7 @@
+const { test, expect } = require('@playwright/test')
+
+test.describe('Create Elements', () => {
+	test('Creation of Elements in the Formulize Forms from the prior test', async ({ page }) => {
+
+	})
+});
