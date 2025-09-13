a0ad508c9878202f1fa09122c936c51c2cf81b65
Julian Egelstaff <julian@formulize.net>
2025-09-13 01:11:06 -0400

    First pass at form and element creation. Needs more elements, etc.

--- tests/e2e/formulize-core/020-create-elements.spec.js
+++
@@ -1,7 +0,0 @@
-const { test, expect } = require('@playwright/test')
-
-test.describe('Create Elements', () => {
-	test('Creation of Elements in the Formulize Forms from the prior test', async ({ page }) => {
-
-	})
-});
