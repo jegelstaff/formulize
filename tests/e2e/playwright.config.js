e8453cde33095f22d23ee7bc4aa4e4de00fade4a
Julian Egelstaff <julian@formulize.net>
2025-09-13 11:55:29 -0400

    Increase timeout

--- tests/e2e/playwright.config.js
+++ tests/e2e/playwright.config.js
@@ -24,7 +24,7 @@ module.exports = defineConfig({
     ['html', {  outputFolder: './test-report' }]
   ],
 	/* Adjust the timeout for slow tests */
-	timeout: 120000,
+	timeout: 300000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
