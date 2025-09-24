090b70016a069f132236a448c92b30bbe8e05b77
Julian Egelstaff <julian@formulize.net>
2025-09-24 00:43:00 -0400

    Fixing how canHaveMultipleValues is set for autocompletes and listboxes

--- modules/formulize/class/autocompleteElement.php
+++ modules/formulize/class/autocompleteElement.php
@@ -42,7 +42,7 @@ class formulizeAutocompleteElement extends formulizeSelectElement {
 		$this->overrideDataType = ""; // use this to set a datatype for the database if you need the element to always have one (like 'date').  set needsDataType to false if you use this.
 		$this->adminCanMakeRequired = true; // set to true if the webmaster should be able to toggle this element as required/not required
 		$this->alwaysValidateInputs = false; // set to true if you want your custom validation function to always be run.  This will override any required setting that the webmaster might have set, so the recommendation is to set adminCanMakeRequired to false when this is set to true.
-		$this->canHaveMultipleValues = false;
+		// $this->canHaveMultipleValues = false; // set by setCanHaveMultipleValues method in the parent class, which is called as part of 'get' operation (in _setElementProperties method in elements.php)
 		$this->hasMultipleOptions = true;
 		$this->isLinked = false; // set to true if this element can have linked values
 	}
