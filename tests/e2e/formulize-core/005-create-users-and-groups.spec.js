7ae36270c6cb017c45b72ad79d9c2610d74bc32e
Arvin Singla <arvin.singla@gmail.com>
2025-09-24 08:53:42 -0400

    Changes for testing in github actions.

--- tests/e2e/formulize-core/005-create-users-and-groups.spec.js
+++ tests/e2e/formulize-core/005-create-users-and-groups.spec.js
@@ -13,7 +13,7 @@ test.describe('Create Groups', () => {
   	await page.getByRole('link', { name: 'Groups Groups' }).click();
   })
 
-	test('Create Ancient History group', async ({ page }) => {
+	test.only('Create Ancient History group', async ({ page }) => {
 		await page.locator('#name').fill('Ancient History');
 		await page.getByRole('button', { name: 'Create New Group' }).click();
 		await expect(page.getByRole('rowgroup')).toContainText('Ancient History');
@@ -32,71 +32,71 @@ test.describe('Create Groups', () => {
 	})
 })
 
-test.describe('Create Users', () => {
+// test.describe('Create Users', () => {
 
-	test.beforeEach(async ({ page }) => {
-		await login(page, E2E_TEST_ADMIN_USERNAME, E2E_TEST_ADMIN_PASSWORD);
-		await page.getByRole('link', { name: 'Admin' }).click();
-  	await page.getByRole('link', { name: 'arrow System' }).click();
-		await page.getByRole('link', { name: 'Edit Users Edit Users' }).click();
-  })
+// 	test.beforeEach(async ({ page }) => {
+// 		await login(page, E2E_TEST_ADMIN_USERNAME, E2E_TEST_ADMIN_PASSWORD);
+// 		await page.getByRole('link', { name: 'Admin' }).click();
+//   	await page.getByRole('link', { name: 'arrow System' }).click();
+// 		await page.getByRole('link', { name: 'Edit Users Edit Users' }).click();
+//   })
 
-	test('Create Ancient History user', async ({ page }) => {
+// 	test('Create Ancient History user', async ({ page }) => {
 
-		await page.locator('#login_name').fill('ahstaff');
-		await page.locator('#uname').fill('Ancient History Staff');
-		await page.locator('#email').fill('ahstaff@museum.formulize.net');
- 		await page.locator('#password').fill('12345');
-		await page.locator('#vpass').fill('12345');
-		await page.getByRole('radio', { name: 'Active', exact: true }).check();
-		await page.locator('#groups').selectOption(['Ancient History', 'Registered Users']);
-		await page.getByRole('button', { name: 'Save changes' }).click();
-		await page.locator('#buttonbar').getByRole('link', { name: 'Find users' }).click();
-		await page.locator('#user_email').fill('ahstaff@museum.formulize.net');
-		await page.getByRole('button', { name: 'Submit' }).click();
-  	await expect(page.getByRole('rowgroup')).toContainText('Ancient History Staff');
-	})
-	test('Create Modern History user', async ({ page }) => {
-		await page.locator('#login_name').fill('mhstaff');
-		await page.locator('#uname').fill('Modern History Staff');
-		await page.locator('#email').fill('mhstaff@museum.formulize.net');
-		await page.locator('#password').fill('12345');
-		await page.locator('#vpass').fill('12345');
-		await page.getByRole('radio', { name: 'Active', exact: true }).check();
-		await page.locator('#groups').selectOption(['Modern History', 'Registered Users']);
-		await page.getByRole('button', { name: 'Save changes' }).click();
-		await page.locator('#buttonbar').getByRole('link', { name: 'Find users' }).click();
-		await page.locator('#user_email').fill('mhstaff@museum.formulize.net');
-		await page.getByRole('button', { name: 'Submit' }).click();
-  	await expect(page.getByRole('rowgroup')).toContainText('Modern History Staff');
-	})
-	test('Create Curator One user', async ({ page }) => {
-		await page.locator('#login_name').fill('curator1');
-		await page.locator('#uname').fill('Curator One');
-		await page.locator('#email').fill('c1@museum.formulize.net');
-		await page.locator('#password').fill('12345');
-		await page.locator('#vpass').fill('12345');
-		await page.getByRole('radio', { name: 'Active', exact: true }).check();
-		await page.locator('#groups').selectOption(['Ancient History', 'Curators', 'Modern History', 'Registered Users']);
-		await page.getByRole('button', { name: 'Save changes' }).click();
-		await page.locator('#buttonbar').getByRole('link', { name: 'Find users' }).click();
-		await page.locator('#user_email').fill('c1@museum.formulize.net');
-		await page.getByRole('button', { name: 'Submit' }).click();
-  	await expect(page.getByRole('rowgroup')).toContainText('Curator One');
-	})
+// 		await page.locator('#login_name').fill('ahstaff');
+// 		await page.locator('#uname').fill('Ancient History Staff');
+// 		await page.locator('#email').fill('ahstaff@museum.formulize.net');
+//  		await page.locator('#password').fill('12345');
+// 		await page.locator('#vpass').fill('12345');
+// 		await page.getByRole('radio', { name: 'Active', exact: true }).check();
+// 		await page.locator('#groups').selectOption(['Ancient History', 'Registered Users']);
+// 		await page.getByRole('button', { name: 'Save changes' }).click();
+// 		await page.locator('#buttonbar').getByRole('link', { name: 'Find users' }).click();
+// 		await page.locator('#user_email').fill('ahstaff@museum.formulize.net');
+// 		await page.getByRole('button', { name: 'Submit' }).click();
+//   	await expect(page.getByRole('rowgroup')).toContainText('Ancient History Staff');
+// 	})
+// 	test('Create Modern History user', async ({ page }) => {
+// 		await page.locator('#login_name').fill('mhstaff');
+// 		await page.locator('#uname').fill('Modern History Staff');
+// 		await page.locator('#email').fill('mhstaff@museum.formulize.net');
+// 		await page.locator('#password').fill('12345');
+// 		await page.locator('#vpass').fill('12345');
+// 		await page.getByRole('radio', { name: 'Active', exact: true }).check();
+// 		await page.locator('#groups').selectOption(['Modern History', 'Registered Users']);
+// 		await page.getByRole('button', { name: 'Save changes' }).click();
+// 		await page.locator('#buttonbar').getByRole('link', { name: 'Find users' }).click();
+// 		await page.locator('#user_email').fill('mhstaff@museum.formulize.net');
+// 		await page.getByRole('button', { name: 'Submit' }).click();
+//   	await expect(page.getByRole('rowgroup')).toContainText('Modern History Staff');
+// 	})
+// 	test('Create Curator One user', async ({ page }) => {
+// 		await page.locator('#login_name').fill('curator1');
+// 		await page.locator('#uname').fill('Curator One');
+// 		await page.locator('#email').fill('c1@museum.formulize.net');
+// 		await page.locator('#password').fill('12345');
+// 		await page.locator('#vpass').fill('12345');
+// 		await page.getByRole('radio', { name: 'Active', exact: true }).check();
+// 		await page.locator('#groups').selectOption(['Ancient History', 'Curators', 'Modern History', 'Registered Users']);
+// 		await page.getByRole('button', { name: 'Save changes' }).click();
+// 		await page.locator('#buttonbar').getByRole('link', { name: 'Find users' }).click();
+// 		await page.locator('#user_email').fill('c1@museum.formulize.net');
+// 		await page.getByRole('button', { name: 'Submit' }).click();
+//   	await expect(page.getByRole('rowgroup')).toContainText('Curator One');
+// 	})
 
-	test('Create Curator Two user', async ({ page }) => {
-		await page.locator('#login_name').fill('curator2');
-		await page.locator('#uname').fill('Curator Two');
-		await page.locator('#email').fill('c2@museum.formulize.net');
-		await page.locator('#password').fill('12345');
-		await page.locator('#vpass').fill('12345');
-		await page.getByRole('radio', { name: 'Active', exact: true }).check();
-		await page.locator('#groups').selectOption(['Ancient History', 'Curators', 'Modern History', 'Registered Users']);
-		await page.getByRole('button', { name: 'Save changes' }).click();
-		await page.locator('#buttonbar').getByRole('link', { name: 'Find users' }).click();
-		await page.locator('#user_email').fill('c2@museum.formulize.net');
-		await page.getByRole('button', { name: 'Submit' }).click();
-  	await expect(page.getByRole('rowgroup')).toContainText('Curator Two');
-	})
-})
+// 	test('Create Curator Two user', async ({ page }) => {
+// 		await page.locator('#login_name').fill('curator2');
+// 		await page.locator('#uname').fill('Curator Two');
+// 		await page.locator('#email').fill('c2@museum.formulize.net');
+// 		await page.locator('#password').fill('12345');
+// 		await page.locator('#vpass').fill('12345');
+// 		await page.getByRole('radio', { name: 'Active', exact: true }).check();
+// 		await page.locator('#groups').selectOption(['Ancient History', 'Curators', 'Modern History', 'Registered Users']);
+// 		await page.getByRole('button', { name: 'Save changes' }).click();
+// 		await page.locator('#buttonbar').getByRole('link', { name: 'Find users' }).click();
+// 		await page.locator('#user_email').fill('c2@museum.formulize.net');
+// 		await page.getByRole('button', { name: 'Submit' }).click();
+//   	await expect(page.getByRole('rowgroup')).toContainText('Curator Two');
+// 	})
+// })
