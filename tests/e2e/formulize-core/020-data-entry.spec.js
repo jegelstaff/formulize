039a0669e36c4d6b3590102d2c8cd3bdf934f18c
Julian Egelstaff <julian@formulize.net>
2025-10-14 01:54:27 +0300

    Save and Close is a link not a button!

--- tests/e2e/formulize-core/020-data-entry.spec.js
+++ tests/e2e/formulize-core/020-data-entry.spec.js
@@ -39,7 +39,7 @@ test.describe('Data Entry for Collections', () => {
 	  await page.getByRole('checkbox', { name: 'Children' }).check();
   	await page.getByRole('checkbox', { name: 'Adults' }).check();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create Modern History collection', async ({ page }) => {
@@ -51,7 +51,7 @@ test.describe('Data Entry for Collections', () => {
 	  await page.getByRole('checkbox', { name: 'Children' }).check();
   	await page.getByRole('checkbox', { name: 'Adults' }).check();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create Coins collection', async ({ page }) => {
@@ -62,7 +62,7 @@ test.describe('Data Entry for Collections', () => {
   	await page.getByRole('textbox', { name: 'Name *' }).fill('Coins');
    	await page.getByRole('checkbox', { name: 'Adults' }).check();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create Weapons collection', async ({ page }) => {
@@ -74,7 +74,7 @@ test.describe('Data Entry for Collections', () => {
 		await page.getByRole('checkbox', { name: 'Children' }).check();
    	await page.getByRole('checkbox', { name: 'Adults' }).check();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 })
 
@@ -99,7 +99,7 @@ test.describe('Data Entry for Donors', () => {
   	await page.getByRole('combobox').selectOption('Quebec');
   	await page.locator('input[name="de_2_new_23"]').fill('G1A 0A2');
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create Emilie donor', async ({ page }) => {
@@ -114,7 +114,7 @@ test.describe('Data Entry for Donors', () => {
   	await page.getByRole('combobox').selectOption('Quebec');
   	await page.locator('input[name="de_2_new_23"]').fill('G1A 0A2');
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create Freeform donor', async ({ page }) => {
@@ -127,7 +127,7 @@ test.describe('Data Entry for Donors', () => {
   	await page.getByRole('combobox').selectOption('Ontario');
   	await page.locator('input[name="de_2_new_23"]').fill('M4E 3X2');
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 })
@@ -156,7 +156,7 @@ test.describe('Data entry for Artifacts as ahstaff', () => {
 		await page.getByRole('checkbox', { name: 'Ancient History' }).check();
 		await page.getByRole('checkbox', { name: 'Coins' }).check();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create Persian necklace', async ({ page }) => {
@@ -173,7 +173,7 @@ test.describe('Data entry for Artifacts as ahstaff', () => {
 		await page.getByRole('slider', { name: 'Condition' }).fill('9');
 		await page.getByRole('checkbox', { name: 'Ancient History' }).check();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create Chinese Sword', async ({ page }) => {
@@ -191,7 +191,7 @@ test.describe('Data entry for Artifacts as ahstaff', () => {
 		await page.getByRole('checkbox', { name: 'Ancient History' }).check();
 		await page.getByRole('checkbox', { name: 'Weapons' }).check();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create Egyptian Chariot', async ({ page }) => {
@@ -208,7 +208,7 @@ test.describe('Data entry for Artifacts as ahstaff', () => {
 		await page.getByRole('checkbox', { name: 'Ancient History' }).check();
 		await page.getByRole('checkbox', { name: 'Weapons' }).check();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create Babylonian Spoon', async ({ page }) => {
@@ -225,7 +225,7 @@ test.describe('Data entry for Artifacts as ahstaff', () => {
 		await page.getByRole('slider', { name: 'Condition' }).fill('8');
 		await page.getByRole('checkbox', { name: 'Ancient History' }).check();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 })
 
@@ -252,7 +252,7 @@ test.describe('Data entry for Artifacts as mhstaff', () => {
 		await page.getByRole('slider', { name: 'Condition' }).fill('9');
 		await page.getByRole('checkbox', { name: 'Modern History' }).check();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create French Musket', async ({ page }) => {
@@ -269,7 +269,7 @@ test.describe('Data entry for Artifacts as mhstaff', () => {
 		await page.getByRole('checkbox', { name: 'Modern History' }).check();
 		await page.getByRole('checkbox', { name: 'Weapons' }).check();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create Japanese Coin', async ({ page }) => {
@@ -287,7 +287,7 @@ test.describe('Data entry for Artifacts as mhstaff', () => {
 		await page.getByRole('checkbox', { name: 'Modern History' }).check();
 		await page.getByRole('checkbox', { name: 'Coins' }).check();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create American Bicycle', async ({ page }) => {
@@ -303,7 +303,7 @@ test.describe('Data entry for Artifacts as mhstaff', () => {
 		await page.getByRole('slider', { name: 'Condition' }).fill('4');
 		await page.getByRole('checkbox', { name: 'Modern History' }).check();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create Polynesian Canoe', async ({ page }) => {
@@ -320,7 +320,7 @@ test.describe('Data entry for Artifacts as mhstaff', () => {
 		await page.getByRole('slider', { name: 'Condition' }).fill('8');
 		await page.getByRole('checkbox', { name: 'Modern History' }).check();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create Viking Silver Armband', async ({ page }) => {
@@ -337,7 +337,7 @@ test.describe('Data entry for Artifacts as mhstaff', () => {
 		await page.getByRole('slider', { name: 'Condition' }).fill('9');
 		await page.getByRole('checkbox', { name: 'Modern History' }).check();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 })
 
@@ -402,7 +402,7 @@ test.describe('Data entry for Exhibits', () => {
 		await page.locator('input[type=text]').nth(2).fill('vik');
 		await page.getByText('Viking Silver Armband').click();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create Ancient Wonders Exhibit', async ({ page }) => {
@@ -426,7 +426,7 @@ test.describe('Data entry for Exhibits', () => {
 		await page.locator('input[type=text]').nth(2).fill('bab');
 		await page.getByText('Babylonian Spoon').click();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create Modern Amazements Exhibit', async ({ page }) => {
@@ -452,7 +452,7 @@ test.describe('Data entry for Exhibits', () => {
 		await page.locator('input[type=text]').nth(2).fill('vik');
 		await page.getByText('Viking Silver Armband').click();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create Heroic and Horrible Hand Weapons Exhibit', async ({ page }) => {
@@ -469,7 +469,7 @@ test.describe('Data entry for Exhibits', () => {
 		await page.locator('input[type=text]').nth(2).fill('fren');
 		await page.getByText('French Musket').click();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 
 	test('Create Pennies from the Past Exhibit', async ({ page }) => {
@@ -487,7 +487,7 @@ test.describe('Data entry for Exhibits', () => {
 		await page.locator('input[type=text]').nth(2).fill('japa');
 		await page.getByText('Japanese Coin').click();
 		await saveFormulizeForm(page);
-		await page.getByRole('button', { name: 'Save and Close' }).click(); // necessary to clear entry locks
+		await page.getByRole('link', { name: 'Save and Close' }).click(); // necessary to clear entry locks
 	})
 })
 
