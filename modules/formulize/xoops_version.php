ff4c4877424a51beadbfaed32985357466453520
Julian Egelstaff <julian@formulize.net>
2026-01-20 12:27:34 -0500

    Reverting version to 8.2-beta [SKIP TESTS]

--- modules/formulize/xoops_version.php
+++ modules/formulize/xoops_version.php
@@ -31,7 +31,7 @@
 
 $modversion = array(
 	'name' => _MI_formulize_NAME,
-	'version' => "8.13",
+	'version' => "8.2-beta",
 	'min_php_version' => "8.1",
 	'min_php_version_id' => "80100",
 	'description' => _MI_formulize_DESC,
