a400738501dddb1f52ee6baaccd05d43d2d60fd4
Julian Egelstaff <julian@formulize.net>
2026-02-04 17:54:38 -0500

    Basic rough in for managing groups as part of user editing

--- modules/formulize/language/french/main.php
+++ modules/formulize/language/french/main.php
@@ -666,6 +666,7 @@ define("_formulize_USERACCOUNTTIMEZONE", "Fuseau horaire");
 define("_formulize_USERACCOUNTPHONE", "Numéro de téléphone");
 define("_formulize_USERACCOUNTNOTIFICATIONMETHOD", "Méthode de notification");
 define("_formulize_USERACCOUNT2FA", "Paramètres d'authentification à deux facteurs");
+define("_formulize_USERACCOUNTGROUPMEMBERSHIP", "Appartenance au groupe");
 define("_formulize_USERACCOUNT_2FAOPTION_EMAIL", "Envoyez un code par email");
 define("_formulize_USERACCOUNT_2FAOPTION_SMS", "Envoyez un code par SMS");
 define("_formulize_USERACCOUNT_2FAOPTION_AUTHAPP", "Utilisez une application d'authentification");
