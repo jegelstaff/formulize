83a1eb7960a03452ca69b9473291474e146dd982
Adam <mail@adamkhafagy.com>
2026-07-14 15:36:34 -0400

    Updated theme files list to be dynamic, based on the files in that installation's folder.

--- modules/formulize/admin/themeeditor.php
+++ modules/formulize/admin/themeeditor.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Formulize Log Viewer
- * Admin page for viewing and filtering system log files
+ * Formulize Theme Editor
+ * Admin page for viewing and editing theme files
  */
 
 // Only webmasters can access this page
@@ -22,6 +22,36 @@ $adminPage['themes'] = icms_view_theme_Factory::getThemesList();
 // Site's current default theme
 global $xoopsConfig;
 $adminPage['default_theme'] = $xoopsConfig['theme_set'];
+
+// Which theme's files to show: whatever was picked in the select, falling back to the site's default theme
+$requestedTheme = isset($_GET['theme']) ? $_GET['theme'] : (isset($_POST['theme']) ? $_POST['theme'] : '');
+$adminPage['selected_theme'] = isset($adminPage['themes'][$requestedTheme]) ? $requestedTheme : $adminPage['default_theme'];
+
+// All the files inside the selected theme's folder, so the picklist and file list stay in sync
+$adminPage['theme_files'] = formulize_themeeditor_getThemeFiles(ICMS_THEME_PATH . '/' . $adminPage['selected_theme']);
+
+/**
+ * Recursively list every file inside a theme's folder
+ * @param string themeDir - full filesystem path to the theme's folder
+ * @return array Sorted list of file paths, relative to the theme's folder, using forward slashes
+ */
+function formulize_themeeditor_getThemeFiles($themeDir) {
+    $files = array();
+    if(!is_dir($themeDir)) {
+        return $files;
+    }
+    $iterator = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($themeDir, FilesystemIterator::SKIP_DOTS)
+    );
+    foreach($iterator as $fileInfo) {
+        if($fileInfo->isFile()) {
+            $relativePath = substr($fileInfo->getPathname(), strlen($themeDir) + 1);
+            $files[] = str_replace('\\', '/', $relativePath);
+        }
+    }
+    sort($files, SORT_STRING);
+    return $files;
+}
 
 
 
