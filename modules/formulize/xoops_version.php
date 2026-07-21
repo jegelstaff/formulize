6eacd331e0d65c267925c570d44eee9a6c583db1
Julian Egelstaff <julian@formulize.net>
2026-07-21 19:33:59 -0400

    Bumping DB version number

--- modules/formulize/xoops_version.php
+++ modules/formulize/xoops_version.php
@@ -39,7 +39,7 @@ $modversion = array(
 	'license' => "GPL-2.0",
 	'image' => "images/formulize.gif",
 	'dirname' => "formulize",
-	'dbversion' => 11,
+	'dbversion' => 12,
 	'onUpdate' => "include/on_update.php"
 );
 
